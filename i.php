ff15ef5c753aba7a86ff6334fdd19634771bfaf4
Andrew Berls <andrew.berls@gmail.com>
2011-10-04 09:32:47 -0700

    Removed conditional comments to test production environment exclusion

--- i.php
+++ i.php
@@ -1,6 +1,6 @@
 <?php
-	#if($_SERVER['SERVER_NAME'] != 'www.soc.ucsb.edu')
+	if($_SERVER['SERVER_NAME'] != 'www.soc.ucsb.edu')
 		phpinfo();
-	#else
-	#	exit('For development environment only.');
+	else
+		exit('For development environment only.');
 ?>
