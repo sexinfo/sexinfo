6fa82601dd677c635a234d922825f70eb421cd30
Andrew Berls <andrew.berls@gmail.com>
2012-01-23 15:13:02 -0800

    Tidying up banner and temporarily fixing border glitch

--- sites/all/themes/magazeen_lite/templates/page--front.tpl.php
+++ sites/all/themes/magazeen_lite/templates/page--front.tpl.php
@@ -81,24 +81,22 @@
 </div> <!-- /header -->
 
 <div id="ucbanner">
-    <banner>
-        <a href="http://www.ucsb.edu">University of California Santa Barbara<a/>
-    </banner>
-    <style type="text/css">
-        banner {
-            font-family: "Comic Sans MS";
-            color: #FFFFFF;
-            font-size: 110%;
-        }
-        
-        #ucbanner {
-        background: black;
-        height: 20px;
-        text-align: center;
-        }
-
-    </style>
+     <a href="http://www.ucsb.edu">University of California, Santa Barbara<a/>  
 </div><!-- /ucbanner -->
+
+<style type="text/css">
+
+	#navigation {
+		border-top: none !important;
+	}
+	#ucbanner {
+		background: black;
+		color: #f23183;
+		height: 20px;
+		padding: 5px;
+		text-align: center;
+	}
+</style>
 
 <!-- ______________________ NAVIGATION _______________________ -->
 
@@ -310,6 +308,28 @@
 						<h2>Recent Questions</h2>						
 						
 						<div class="column-container dark">
+						
+							<div class="question">
+								<h4>What Happened to Me?</h4>							
+								<p class="date">May 23, 2011</p>
+								<p>Question: Dear Sexperts, I stayed with a friend and family member in a hotel room. We were only making out and touching. I then passed out completely for at least 2-3 hours. I had been drinking, but I was not drunk. When we got up from the bed, there was blood on the mattress. Since the sheets were off, I did not know if it was possible for me to &hellip;</p>
+								<a href="/sexinfo/node/$nid" class="readmore">Read More &raquo;</a>
+							</div><!-- .question (STATIC) -->
+							
+							<div class="question">
+								<h4>Should I Let My Partner Cheat?</h4>							
+								<p class="date">May 23, 2011</p>
+								<p>Dear Sexperts, My girlfriend wants to have sex with another guy. Just sex. Just once. She wants to know what it's like to cheat. What do I do? -Male, 22, California Answer: It is not uncommon for partners to experience curiosity and to have desires outside of their relationship. However, every relationship is unique. Some prefer an open relatio &hellip;</p>
+								<a href="/sexinfo/node/$nid" class="readmore">Read More &raquo;</a>
+							</div><!-- .question (STATIC) -->
+							
+							<div class="question">
+								<h4>First Time Sex Worries?</h4>							
+								<p class="date">May 23, 2011</p>
+								<p>Question Dear Sexperts, I am 19 and had sex for the first time late last night. It was harder than expected to get it in. Can stress or other thoughts influence the effect? Also, I realized he took off the condom before we finally got it in but he did pull out...I've been on birth control for a few years, should I be worried about getting pregnant and &hellip;</p>
+								<a href="/sexinfo/node/$nid" class="readmore">Read More &raquo;</a>
+							</div><!-- .question (STATIC) -->
+									
 							<?php				
 								// Recent Questions module
 								// Displays the three most recently authored Q/A's and a link to the full node														
@@ -330,8 +350,8 @@
 								// This is an absolutely horrible way to go about things.
 								// Drupal provides DB wrapper functions that I can't figure out for the life of me, and I'm
 								// pretty sure this sort of DB code should never be in a template file anyways. Bummer.								
-								mysql_connect("localhost", "sexweb00m", "249APWan") or die("Could not connect: " . mysql_error());
-								mysql_select_db("sexweb00");
+								//mysql_connect("localhost", "sexweb00m", "249APWan") or die("Could not connect: " . mysql_error());
+								//mysql_select_db("sexweb00");
 								/*
 								 * Question title+timestamp is stored in table:node
 								 * but the question body is stored in table:field_data_field_question,
@@ -339,6 +359,7 @@
 								 * from both tables.
 								 * See http://www.codinghorror.com/blog/2007/10/a-visual-explanation-of-sql-joins.html
 								*/
+								/*
 								$query = "
 									SELECT * FROM node										
 									INNER JOIN field_data_field_question
@@ -347,10 +368,10 @@
 									LIMIT 3
 								";
 								$result = mysql_query($query) or die(mysql_error());
-								
+								*/
 								// Note that = is used in the loop here instead of == which is very intentional
 								// See http://www.tizag.com/mysqlTutorial/mysqlfetcharray.php
-								while($row = mysql_fetch_array($result) ) {
+								/*while($row = mysql_fetch_array($result) ) {
 									// Loop through returned question rows
 									// Initialize content variables to be passed into content_tag()
 									$nid = $row['nid'];
@@ -358,6 +379,7 @@
 									$title = $row['title'];
 									$teaser = slice_teaser($row['field_question_value']);
 									$node_path = "/sexinfo/node/"; // Read More link: ex href="/sexinfo/node/28"
+									*/
 									/*
 									 - OUTPUTTED CODE STRUCTURE -
 									<div class="question">
@@ -365,15 +387,15 @@
 										<p class="date">October 30, 2011</p>
 										<p>Body of the question (Not the answer)</p>
 										<a href="/sexinfo/node/$nid" class="readmore">Read More &raquo;</a>
-									</div>									
+									</div>						
 									*/									
-									print open_tag("div", array("class" => "question"));
+									/*print open_tag("div", array("class" => "question"));
 										content_tag("h4", $title);
 										content_tag("p", $time, array("class" => "date"));																			
 										content_tag("p", $teaser);
 										content_tag("a", "Read More &raquo;", array("href" => $node_path . $nid, "class" => "readmore"));								
-									print close_tag("div");									
-								}															
+									print close_tag("div");*/							
+								//}															
 							?>
 							
 						</div><!-- .column-container.dark -->						
