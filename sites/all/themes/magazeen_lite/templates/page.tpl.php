9ec6e8b917800f65b75f8e54dddb4031b5f3d3af
Andrew Berls <andrew.berls@gmail.com>
2012-04-20 15:37:31 -0700

    Updating footer login

--- sites/all/themes/magazeen_lite/templates/page.tpl.php
+++ sites/all/themes/magazeen_lite/templates/page.tpl.php
@@ -106,10 +106,10 @@
   <div class="container clearfix">
   
 	<h4>About SexInfoOnline</h4>
-		<p><?print '<a href="'. check_url($front_page) .'">';?>SexInfo Online</a> is a website devoted to comprehensive sex education based on the best research we have to date. The site is maintained by university students from the University of California, Santa Barbara who have studied advanced topics in human sexuality.</p>
-		<p>Our primary goal is to ensure that people around the world have access to useful and accurate information about all aspects of human sexuality. If you find our site useful, don't forget that your friends, family, and coworkers may find it useful too!</p>
-  
-		<a href="/login" class="login">Writer Login  &raquo;</a>
+    <p><?print '<a href="'. check_url($front_page) .'">';?>SexInfo Online</a> is a website devoted to comprehensive sex education based on the best research we have to date. The site is maintained by university students from the University of California, Santa Barbara who have studied advanced topics in human sexuality.</p>
+    <p>Our primary goal is to ensure that people around the world have access to useful and accurate information about all aspects of human sexuality. If you find our site useful, don't forget that your friends, family, and coworkers may find it useful too!</p>
+
+    <a href="<?php print $base_path . "login" ?>" class="login">Writer Login &raquo;</a>
   
 	<!--
     <div id="footer-left">
