a585118b4acfd0385dbfdbe9b4f6f5a383b8a446
Andrew Berls <andrew.berls@gmail.com>
2012-06-08 15:06:45 -0700

    Social buttons on all pages

--- sites/all/themes/magazeen_lite/templates/page.tpl.php
+++ sites/all/themes/magazeen_lite/templates/page.tpl.php
@@ -23,6 +23,14 @@
     <div id="searchform-header">
      	<?php print render($page['search_area']); ?>
    	</div>
+
+    <div class="social-buttons">
+      <a class="soc-yt" href="http://www.youtube.com/user/UCSBSexInfoOnline">YouTube</a>
+      <a class="soc-fb" href="https://www.facebook.com/SexInfoOnline">Facebook</a>
+      <iframe src="//www.facebook.com/plugins/like.php?href=http%3A%2F%2Fwww.sexinfoonline.com&amp;send=false&amp;layout=button_count&amp;width=50&amp;show_faces=true&amp;action=like&amp;colorscheme=light&amp;font=lucida+grande&amp;height=21&amp;appId=228872410538762" scrolling="no" frameborder="0" style="border:none; border-height:10px; padding-top:5px; overflow:hidden; width:75px; height:21px;" allowtransparency="true"></iframe>  
+    <!-- <a class="soc-rss" href="#">RSS</a> -->
+    </div><!-- .social-buttons -->
+
   </div> <!-- /header-container -->    
 </div> <!-- /header -->
 
