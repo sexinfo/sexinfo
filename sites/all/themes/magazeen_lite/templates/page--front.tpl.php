ffd619f5be9616c8ca7b7bccaf9b5598808c7268
Andrew Berls <andrew.berls@gmail.com>
2012-09-29 14:22:35 -0700

    Enabling questions and adding link to front banner

--- sites/all/themes/magazeen_lite/templates/page--front.tpl.php
+++ sites/all/themes/magazeen_lite/templates/page--front.tpl.php
@@ -29,20 +29,20 @@
     <div class="social-buttons">
       <a class="soc-yt" href="http://www.youtube.com/user/UCSBSexInfoOnline">YouTube</a>
       <a class="soc-fb" href="https://www.facebook.com/SexInfoOnline">Facebook</a>
-      <iframe src="//www.facebook.com/plugins/like.php?href=http%3A%2F%2Fwww.sexinfoonline.com&amp;send=false&amp;layout=button_count&amp;width=50&amp;show_faces=true&amp;action=like&amp;colorscheme=light&amp;font=lucida+grande&amp;height=21&amp;appId=228872410538762" scrolling="no" frameborder="0" style="border:none; border-height:10px; padding-top:5px; overflow:hidden; width:75px; height:21px;" allowtransparency="true"></iframe>  
+      <iframe src="//www.facebook.com/plugins/like.php?href=http%3A%2F%2Fwww.sexinfoonline.com&amp;send=false&amp;layout=button_count&amp;width=50&amp;show_faces=true&amp;action=like&amp;colorscheme=light&amp;font=lucida+grande&amp;height=21&amp;appId=228872410538762" scrolling="no" frameborder="0" style="border:none; border-height:10px; padding-top:5px; overflow:hidden; width:75px; height:21px;" allowtransparency="true"></iframe>
     <!-- <a class="soc-rss" href="#">RSS</a> -->
     </div><!-- .social-buttons -->
 
-  </div> <!-- /header-container -->    
+  </div> <!-- /header-container -->
 </div> <!-- /header -->
 
 <!--<div class="banner uc-banner">
-     <a href="http://www.ucsb.edu">University of California<a/>  
+     <a href="http://www.ucsb.edu">University of California<a/>
 </div>--><!-- .banner.uc-banner -->
 
 <div class="banner new-banner">
-	Welcome to the new SexInfoOnline! We've changed things around a bit.
-  The new site is more stable and easier to navigate, and we hope you like it as much as we do.
+	The Sexperts are back and answering questions again!
+  <a href="<?php print $base_path . "ask-sexperts" ?>">We'd love to hear from you &raquo;</a>
 </div>
 
 <!-- ______________________ NAVIGATION _______________________ -->
@@ -66,24 +66,24 @@
 
 			<?php if ($breadcrumb): print $breadcrumb; endif; ?>
 
-			<?php 
+			<?php
 				# View and Edit buttons
-				# if ($tabs): print '<div id="tabs-wrapper" class="clear-block">' . render($tabs) .'</div>'; endif; 
+				# if ($tabs): print '<div id="tabs-wrapper" class="clear-block">' . render($tabs) .'</div>'; endif;
 			?>
 
-			<?php 
+			<?php
 				# "has been successfully updated", etc
 				if ($show_messages && $messages): print $messages; endif;
 			?>
 
 			<?php print render($page['help']); ?>
-			
+
 			<div class="clearfix">
-				
+
 				<?php
 					# Main content goes here
 					# This line renders node.tpl.php and the text for this node (which is empty - harcoded instead)
-					# print render($page['content']); 
+					# print render($page['content']);
 				?>
 
         <!--=========================
@@ -96,20 +96,20 @@
                   <?php include "modules/carousel-frames.php" ?>
                 </div><!-- #window-inner -->
               </div><!-- .carousel-window -->
-              
+
               <div id="carousel-nav">
                 <?php include "modules/carousel-links.php" ?>
               </div><!-- .carousel-nav -->
-            
+
             </div><!-- #image-carousel -->
-        </div><!-- .column-container -->	
-				
+        </div><!-- .column-container -->
+
 
         <!--=========================
           TRI BOX MODULES
         =============================-->
 				<div class="box-module-container">
-									
+
           <!-- Left box
           ========================-->
 					<div class="column-third">
@@ -117,7 +117,7 @@
 						<h4 class="module-title">Popular Topics</h4>
 						<div class="module">
 							<div class="node-box">
-								<div class="node-content">									
+								<div class="node-content">
 									<div class="list-half">
 										<a href="<?php print $base_path . "category/abortion" ?>">Abortion</a>
 										<a href="<?php print $base_path . "category/body" ?>">The Body</a>
@@ -134,22 +134,22 @@
 										<a href="<?php print $base_path . "category/sexual-violence" ?>">Sexual Violence</a>
 									</div>
 								</div><!--.node-content-->
-								<div class="node-footer">																								
-									<a href="<?php print $base_path . "category" ?>">All Topics &raquo;</a>									
+								<div class="node-footer">
+									<a href="<?php print $base_path . "category" ?>">All Topics &raquo;</a>
 								</div><!--.node-footer-->
 							</div><!--.node-box-->
 						</div><!-- .module -->
 					</div><!-- .column-third -->
-					
+
 
           <!-- Midle box
           ========================-->
-					<div class="column-third">					
-						<!-- Frequently Asked Questions -->						
+					<div class="column-third">
+						<!-- Frequently Asked Questions -->
 						<h4 class="module-title">Frequently Asked Questions</h4>
 						<div class="module">
 							<div class="node-box">
-								<div class="node-content">									
+								<div class="node-content">
 									<div class="faq-image">
 										<img class="" src="<?php print path_to_theme() . '/images/modules/' . 'masturbation.jpg'; ?>" />
 										<div class="caption-slide">
@@ -157,68 +157,68 @@
 											<p>Masturbation, or self-stimulation of the genitals for pleasure, is not a dangerous or bad activity for men or women. In facts &hellip;
 											<a href="http://www.soc.ucsb.edu/sexinfo/question/faq-masturbation-dangerous" class="more-button">Read More</a></p>
 										</div><!-- .caption-slide -->
-									</div><!-- .faq-image -->								
+									</div><!-- .faq-image -->
 								</div><!--.node-content-->
-								<div class="node-footer">																								
-									<a href="<?php print $base_path . "question" ?>">All Questions &raquo;</a>									
+								<div class="node-footer">
+									<a href="<?php print $base_path . "question" ?>">All Questions &raquo;</a>
 								</div><!--.node-footer-->
 							</div><!--.node-box-->
-						</div><!-- .module -->							
+						</div><!-- .module -->
 					</div><!-- .column-third -->
 
 
           <!-- Right box
           ========================-->
-          <div class="column-third">          
-            <!-- BOX TITLE -->           
+          <div class="column-third">
+            <!-- BOX TITLE -->
             <h4 class="module-title">Ask the Sexperts</h4>
             <div class="module ask-module">
               <div class="node-box">
-                <div class="node-content">                  
+                <div class="node-content">
                   <p>Can't find an answer to your question? Want to send in feedback about the new site? We'd love to hear from you! Click <a href="<?php print $base_path . "ask-sexperts" ?>">here</a> to send a question to the Sexperts.</p>
                 </div><!--.node-content-->
-                <div class="node-footer">                                               
-                  <a href="<?php print $base_path . "ask-sexperts" ?>">Ask a Question &raquo;</a>               
+                <div class="node-footer">
+                  <a href="<?php print $base_path . "ask-sexperts" ?>">Ask a Question &raquo;</a>
                 </div><!--.node-footer-->
               </div><!--.node-box-->
-            </div><!-- .module -->              
+            </div><!-- .module -->
           </div><!-- .column-third -->
-					
+
 				</div><!-- .box-module-container -->
 
-				
+
 			</div><!-- .clearfix -->
-		
-      <?php 
+
+      <?php
       /*
 			<div id="sidebar" class="right">
-				<?php print render($page['sidebar_second']); ?> 
+				<?php print render($page['sidebar_second']); ?>
 			</div><!-- #sidebar.right -->
       */
       ?>
-		
+
 	</div><!-- .container.clearfix -->
 </div><!-- #main.clearfix -->
- 
+
 <!-- ______________________ FOOTER _______________________ -->
 
 <div id="footer">
   <div class="container clearfix">
-  
+
 	 <h4>About SexInfoOnline</h4>
 		<p><?print '<a href="'. check_url($front_page) .'">';?>SexInfoOnline</a> is a website devoted to comprehensive sex education based on the best research we have to date. The site is maintained by university students from the University of California, Santa Barbara who have studied advanced topics in human sexuality.</p>
 		<p>Our primary goal is to ensure that people around the world have access to useful and accurate information about all aspects of human sexuality. If you find our site useful, don't forget that your friends, family, and coworkers may find it useful too!</p>
 
 		<a href="<?php print $base_path . "login" ?>" class="login">Writer Login &raquo;</a>
-  
+
   </div><!-- /footer-container -->
 </div><!-- /footer -->
 
 
 <!-- ______________________ SECONDARY NAVIGATION _______________________ -->
   <div id="snavigation">
-    <div class="container clearfix">	
-	  <p>UCSB SexInfo Copyright &copy; 2012 University of California, Santa Barbara. All Rights Reserved.</p>	  		  	  
+    <div class="container clearfix">
+	  <p>UCSB SexInfo Copyright &copy; 2012 University of California, Santa Barbara. All Rights Reserved.</p>
     </div> <!-- /snavigation-container -->
   </div> <!-- /snavigation -->
 
@@ -226,74 +226,74 @@
 
 <script type="text/javascript">
 	// Code to run on page load
-	
+
 	(function ($) {
-	
+
 		//$(".new-banner").slideDown("slow");
-		
+
 		//----- Image carousel module ---//
 		var loopRotate = true; // A boolean determining whether or not to automatically cycle. Disabled once user clicks one of the nav links.
     var numLinks = 8;
-		
+
 		function rotate(id) {
 			var offset = Math.abs(id-1),
 				distance = 250 * offset; // 250 = hardcoded image height
-			
+
 			$('#window-inner').animate({
 				top: -distance
 			}, 550);
 		}
-		
+
 		$('#carousel-nav a').click(function() {
 			clearInterval(loop); // Disable auto cycle once user clicks
 			$('#carousel-nav a').removeClass('current'); // Remove from all tabs, not just $(this)
 			$(this).addClass("current");
 			var id = $(this).attr('rel'); // Not a very semantic way of doing it, but oh well. TODO - CHANGE TO IDS
-			rotate(id);			
+			rotate(id);
 			return false; //Prevent browser jump to anchor link
 		});
-		
+
 		/* Automatic carousel cycling */
 		var i = 1,
 			links = $("#carousel-nav a"); // Get all of the nav links for looping through later
-				
+
 		function cycle() {
 			// Rotate the image frame
-			rotate(i);			
-			
+			rotate(i);
+
 			// Remove 'current' class from all tabs
 			links.removeClass('current');
-			
+
 			// Loop through all links in nav list
 			// If the link's rel attribute (used as an identifier) matches current frame, add class 'current'
 			$.each(links, function() {
 				if ( $(this).attr("rel") == i ) {
 					$(this).addClass("current");
-				}			
+				}
 			});
-			
-			// Increment the current frame and reset to first if at end 			
+
+			// Increment the current frame and reset to first if at end
 			i++;
-			if (i == (numLinks+1)) { i = 1; }			
+			if (i == (numLinks+1)) { i = 1; }
 		} // end cycle()
-		
+
 		if (loopRotate == true) {
 			// Cycle the frame every 5 seconds if the user hasn't clicked a link
 			var loop = setInterval(cycle, 5000);
 		}
 
-		
+
 		//----- Sliding captions for FAQ module ---//
 		$(".caption-slide").hover(
 			function () {
 				// Hover on - slide up for full view
 				$(this).animate({bottom: '0'}, 300);
-			}, 
+			},
 			function () {
 				// Hover off - slide back down
 				$(this).animate({bottom: '-40px'}, 300);
 			}
 		);
-											  
+
 	})(jQuery);
 </script>
