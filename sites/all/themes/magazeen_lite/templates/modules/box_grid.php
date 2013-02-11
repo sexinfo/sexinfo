08d23e8b7cd8d228e370f65da0e2e96908e31667
Andrew Berls <andrew.berls@gmail.com>
2013-02-10 17:39:37 -0800

    Fixing broken links on home page

--- sites/all/themes/magazeen_lite/templates/modules/box_grid.php
+++ sites/all/themes/magazeen_lite/templates/modules/box_grid.php
@@ -14,20 +14,19 @@
 
         <div class="node-content">
           <div class="list-half">
-            <a href="<?php print $base_path . "category/abortion" ?>">Abortion</a>
+            <a href="<?php print $base_path . "article/overview" ?>">Abortion</a>
             <a href="<?php print $base_path . "category/body" ?>">The Body</a>
             <a href="<?php print $base_path . "category/sexual-orientations" ?>">Sexual Orientations</a>
             <a href="<?php print $base_path . "category/pregnancy" ?>">Pregnancy</a>
-            <a href="<?php print $base_path . "category/sexual-activity" ?>">Sexual Activity</a>
+            <a href="<?php print $base_path . "taxonomy/term/442/all" ?>">Sexual Activity</a>
           </div>
 
           <div class="list-half">
             <a href="<?php print $base_path . "category/sexually-transmitted-infections" ?>">Sexually Transmitted Infections</a>
             <a href="<?php print $base_path . "category/basics-sexuality" ?>">Basics of Sexuality</a>
-            <a href="<?php print $base_path . "category/love-relationships" ?>">Love &amp; Relationships</a>
-            <!-- <a href="<?php #print $base_path . "category/abortion" ?>">Legislation</a> -->
-            <a href="<?php print $base_path . "category/sexual-difficulties" ?>">Sexual Difficulties</a>
-            <a href="<?php print $base_path . "category/sexual-violence" ?>">Sexual Violence</a>
+            <a href="<?php print $base_path . "category/love-relationships/love" ?>">Love &amp; Relationships</a>
+            <a href="<?php print $base_path . "category/sexual-difficulties/male-difficulty" ?>">Sexual Difficulties</a>
+            <a href="<?php print $base_path . "taxonomy/term/447/all" ?>">Sexual Violence</a>
           </div>
         </div><!--.node-content-->
 
@@ -73,7 +72,7 @@
     <div class="module ask-module">
       <div class="node-box">
         <div class="node-content">
-          <p>Can't find an answer to your question? Want to send in feedback about the new site? We'd love to hear from you! Click <a href="<?php print $base_path . "ask-sexperts" ?>">here</a> to send a question to the Sexperts.</p>
+          <p>Can't find an answer to your question? Want to send in feedback on the site? We'd love to hear from you! Click <a href="<?php print $base_path . "ask-sexperts" ?>">here</a> to send a question to the Sexperts.</p>
         </div><!--.node-content-->
         <div class="node-footer">
           <a href="<?php print $base_path . "ask-sexperts" ?>">Ask a Question &raquo;</a>
