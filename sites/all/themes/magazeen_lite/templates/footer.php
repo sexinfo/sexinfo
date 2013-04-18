ff0ae3f42b2cdac7703a7f4bb192fe78943baabb
Joey Cody <jcody@umail.ucsb.edu>
2013-04-17 20:34:26 -0700

    added fontawesome, cleaned footer and ask sexperts pink banner

--- sites/all/themes/magazeen_lite/templates/footer.php
+++ sites/all/themes/magazeen_lite/templates/footer.php
@@ -1,9 +1,21 @@
 <div id="footer">
   <div class="container clearfix">
-
-   <h4>About SexInfoOnline</h4>
-    <p><?print '<a href="'. check_url($front_page) .'">';?>SexInfoOnline</a> is a website devoted to comprehensive sex education based on the best research we have to date. The site is maintained by university students from the University of California, Santa Barbara who have studied advanced topics in human sexuality.</p>
-    <p>Our primary goal is to ensure that people around the world have access to useful and accurate information about all aspects of human sexuality. If you find our site useful, don't forget that your friends, family, and coworkers may find it useful too!</p>
+	  	<div class="left">
+		   	<h4>About SexInfoOnline</h4>
+		    <p><?print '<a href="'. check_url($front_page) .'">';?>SexInfoOnline</a> is a website devoted to comprehensive sex education based on the best research we have to date. Our primary goal is to ensure that people around the world have access to useful and accurate information about all aspects of human sexuality.</p>
+		    <p>The site is maintained by university students from the University of California, Santa Barbara who have studied advanced topics in human sexuality.</p>
+		</div>
+		
+		<div class="right">
+			<h4>Elsewhere on the Web</h4>
+			<ul>
+				<a><li><i class="icon-github-alt"></i> We're open source!</li></a>
+				<a><li><i class="icon-github-alt"></i> We're open source!</li></a>
+				<a><li><i class="icon-github-alt"></i> We're open source!</li></a>
+				<a><li><i class="icon-github-alt"></i> We're open source!</li></a>
+			</ul>
+		</div>
+				
 
     <a href="<?php print $base_path . "login" ?>" class="login">Writer Login &raquo;</a>
 
