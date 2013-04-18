b0eaeaef4c3d25b25752bcc2a32961156bc4affc
Joey Cody <jcody@umail.ucsb.edu>
2013-04-18 02:57:31 -0700

    finished footer, fontawesome and other slight ui tweaks

--- sites/all/themes/magazeen_lite/templates/footer.php
+++ sites/all/themes/magazeen_lite/templates/footer.php
@@ -8,16 +8,18 @@
 		
 		<div class="right">
 			<h4>Elsewhere on the Web</h4>
-			<ul>
-				<a><li><i class="icon-github-alt"></i> We're open source!</li></a>
-				<a><li><i class="icon-github-alt"></i> We're open source!</li></a>
-				<a><li><i class="icon-github-alt"></i> We're open source!</li></a>
-				<a><li><i class="icon-github-alt"></i> We're open source!</li></a>
+			<ul class="social">
+				<a href="https://www.facebook.com/SexInfoOnline"><li><i class="icon-large icon-facebook-sign"></i>Be a Fan on Facebook</li></a>
+				<a href="http://www.youtube.com/user/UCSBSexInfoOnline"><li><i class="icon-large icon-twitter-sign"></i>Follow Us on Twitter</li></a>
+				<a href="https://twitter.com/sexinfoonline"><li><i class="icon-large icon-facetime-video"></i>Visit Our Youtube Channel</li></a>
+				<a href="https://github.com/sexinfo/sexinfo"><li><i class="icon-large icon-github-alt"></i>We're Open Source!</li></a>
 			</ul>
+			
+			<a href="<?php print $base_path . "login" ?>" class="login">Writer Login &raquo;</a>
 		</div>
 				
 
-    <a href="<?php print $base_path . "login" ?>" class="login">Writer Login &raquo;</a>
+    
 
   </div>
 </div><!-- #footer -->
