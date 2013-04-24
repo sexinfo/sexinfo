8ab38f65358b4a47997b2b58ecb503347dc51c05
Julio Garcia <juliogarcia93@gmail.com>
2013-04-24 00:43:33 -0700

    Revert "Changed submit error color and added blur."
    This reverts commit a9b3f9caa0b2cac40fb86b2d4ec0fa6d24f34255.

--- sites/all/themes/magazeen_lite/js/magazeenlite.js
+++ sites/all/themes/magazeen_lite/js/magazeenlite.js
@@ -94,20 +94,18 @@ $(".force-previous a").click(function() {
 $('.webform-client-form').submit(function() {
   var errors = false;
 
-  $(this).find("input:not([type='submit']), textarea").each(function(i, field) {
+  $(this).find("input:not([type='submit'])").each(function(i, field) {
     if ($(field).val() === "") {
       errors = true;
-      $(field).css('border', '1px solid #F23183');
-      $(field).css('color', '#F23183');
+      $(field).css('border', '1px solid #d34332');
+      $(field).css('color', '#d34332');
       $(field).val("This is a required field.");
     }
     $(field).focus(function() {
       $(field).val("");  
     });
-    $(field).blur(function() {
-      $(field).val("This is a required field.");  
-    });
   })
+
 
   return !errors;
 });
