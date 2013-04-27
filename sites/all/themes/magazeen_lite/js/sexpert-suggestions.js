02511d1b422390cd901dc6446fedb5848b34ed05
Mark Nguyen <mpnguyen@umail.ucsb.edu>
2013-04-27 16:25:20 -0700

    Added logging of the list from the html downloaded from AJAX and fixed not reacting to onkeyup

--- sites/all/themes/magazeen_lite/js/sexpert-suggestions.js
+++ sites/all/themes/magazeen_lite/js/sexpert-suggestions.js
@@ -1,5 +1,6 @@
 
 document.onkeydown = check
+document.onkeyup = check;
 
 var previousQuestionText;
 
@@ -11,24 +12,68 @@ function check()
 		var questionText = questionField.value;
 		if (questionText != previousQuestionText) {
 			previousQuestionText = questionText;
-			changed();
+			changed(previousQuestionText);
 		}
 	}
 }
 
 var xmlhttp;
-function changed()
+function changed(text)
 {
+	// If there is already an async request sent, 
+	// abort the previous one because it is no longer relevant
 	if (xmlhttp != null) xmlhttp.abort();
-	xmlhttp = new XMLHttpRequest();
+	
+	// If there hasn't been a request yet, create a new one
+	else {
+		xmlhttp = new XMLHttpRequest();
+		xmlhttp.onreadystatechange = readyStateChange;
+	}
 	console.log(previousQuestionText);
-	xmlhttp.open("GET", "./search/node/" + previousQuestionText, true);
+	xmlhttp.open("GET", "./search/node/" + text, true);
 	xmlhttp.send();
 }
 
 function readyStateChange() {
 	if (xmlhttp.readyState==4 && xmlhttp.status==200)
 	{
-		console.log(xmlhttp.responseText);
+		// Parse the response as a new "document"
+		var tempDocument = document.createElement('div');
+		tempDocument.innerHTML = xmlhttp.responseText;
+		
+		// Extract the list of results from the document
+		var list = getElementByClassname(tempDocument, "search-results node-results");
+
+		// TODO: 
+		// If there are no results, show the top viewed topics
+		// If there are results, show those results
+
+
+		console.log(list.innerHTML);
 	}
+}
+
+function initIFrame() {
+	// TODO: Create an iframe to display the search result
+}
+
+function setiframecontent(html) {
+	// TODO: Set the content of the iframe
+}
+
+
+function getElementByClassname(document, classname) {
+	// Iterate through all elements
+	var elems = document.getElementsByTagName('*'), i;
+	for (i in elems) {
+		// If the class names match, return the element found
+		if( (' ' + elems[i].className + ' ')
+				.indexOf(' ' + classname + ' ') 
+				> -1) {
+			return elems[i];
+		}
+	}
+
+	// Failure of search is to return null
+	return null;
 }
