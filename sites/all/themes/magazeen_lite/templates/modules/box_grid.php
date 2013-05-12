6b292a72729b1f25b79c3bbdafc9b7bf43dccb08
Andrew Berls <andrew.berls@gmail.com>
2013-05-12 14:28:35 -0700

    First implementation of responsive grid. Still a lot to do

--- sites/all/themes/magazeen_lite/templates/modules/box_grid.php
+++ sites/all/themes/magazeen_lite/templates/modules/box_grid.php
@@ -1,84 +1,65 @@
-<!--=========================
-  TRI BOX MODULES
-=============================-->
+<!--Popular Topics -->
+<div class="span4 front-module">
+  <h4 class="module-title">Popular Topics</h4>
 
-<div class="box-module-container">
+  <div class="module-content">
+    <div class="list-half">
+      <a href="<?php print $base_path . "article/overview" ?>">Abortion</a>
+      <a href="<?php print $base_path . "category/body" ?>">The Body</a>
+      <a href="<?php print $base_path . "category/sexual-orientations" ?>">Sexual Orientations</a>
+      <a href="<?php print $base_path . "category/pregnancy" ?>">Pregnancy</a>
+      <a href="<?php print $base_path . "taxonomy/term/442/all" ?>">Sexual Activity</a>
+      <a href="<?php print $base_path . "category/basics-sexuality" ?>">Basics of Sexuality</a>
+    </div>
 
-  <!-- Left box
-  ========================-->
-  <div class="column-third">
-    <!--Popular Topics -->
-    <h4 class="module-title">Popular Topics</h4>
-    <div class="module">
-      <div class="node-box">
+    <div class="list-half">
+      <a href="<?php print $base_path . "article/sti-symptom-chart" ?>">Sexually Transmitted Infections</a>
+      <a href="<?php print $base_path . "category/love-relationships/love" ?>">Love &amp; Relationships</a>
+      <a href="<?php print $base_path . "category/sexual-difficulties/male-difficulty" ?>">Sexual Difficulties</a>
+      <a href="<?php print $base_path . "taxonomy/term/447/all" ?>">Sexual Violence</a>
+    </div>
+  </div>
 
-        <div class="node-content">
-          <div class="list-half">
-            <a href="<?php print $base_path . "article/overview" ?>">Abortion</a>
-            <a href="<?php print $base_path . "category/body" ?>">The Body</a>
-            <a href="<?php print $base_path . "category/sexual-orientations" ?>">Sexual Orientations</a>
-            <a href="<?php print $base_path . "category/pregnancy" ?>">Pregnancy</a>
-            <a href="<?php print $base_path . "taxonomy/term/442/all" ?>">Sexual Activity</a>
-          </div>
-
-          <div class="list-half">
-            <a href="<?php print $base_path . "article/sti-symptom-chart" ?>">Sexually Transmitted Infections</a>
-            <a href="<?php print $base_path . "category/basics-sexuality" ?>">Basics of Sexuality</a>
-            <a href="<?php print $base_path . "category/love-relationships/love" ?>">Love &amp; Relationships</a>
-            <a href="<?php print $base_path . "category/sexual-difficulties/male-difficulty" ?>">Sexual Difficulties</a>
-            <a href="<?php print $base_path . "taxonomy/term/447/all" ?>">Sexual Violence</a>
-          </div>
-        </div><!--.node-content-->
-
-        <div class="node-footer">
-          <a href="<?php print $base_path . "category" ?>">All Topics &raquo;</a>
-        </div><!--.node-footer-->
-
-      </div><!--.node-box-->
-    </div><!-- .module -->
-  </div><!-- .column-third -->
+  <div class="module-footer">
+    <a href="<?php print $base_path . "category" ?>">All Topics &raquo;</a>
+  </div>
+</div>
 
 
-  <!-- Middle box
-  ========================-->
-  <div class="column-third">
-    <!-- Frequently Asked Questions -->
-    <h4 class="module-title">Frequently Asked Questions</h4>
-    <div class="module">
-      <div class="node-box">
-        <div class="node-content">
-          <div class="faq-image">
-            <img class="" src="<?php print path_to_theme() . '/images/modules/' . 'arousal.jpg'; ?>" />
-            <div class="caption-slide">
-              <h3>How Do I Increase Sexual Arousal?</h3>
-              <p>There are many ways to increase your and your partner’s sexual arousal. Sexual arousal is&hellip;
-              <a href="http://www.soc.ucsb.edu/sexinfo/question/faq-how-do-i-increase-sexual-arousal" class="more-button">Read More</a></p>
-            </div><!-- .caption-slide -->
-          </div><!-- .faq-image -->
-        </div><!--.node-content-->
-        <div class="node-footer">
-          <a href="<?php print $base_path . "question" ?>">All Questions &raquo;</a>
-        </div><!--.node-footer-->
-      </div><!--.node-box-->
-    </div><!-- .module -->
-  </div><!-- .column-third -->
+
+<!-- Frequently Asked Questions -->
+<div class="span4 front-module">
+  <h4 class="module-title">Frequently Asked Questions</h4>
+
+  <div class="module-content">
+    <div class="faq-image">
+      <img class="" src="<?php print path_to_theme() . '/images/modules/' . 'arousal.jpg'; ?>" />
+
+      <div class="caption-slide">
+        <h3>How Do I Increase Sexual Arousal?</h3>
+        <p>There are many ways to increase your and your partner’s sexual arousal. Sexual arousal is&hellip;
+        <a href="http://www.soc.ucsb.edu/sexinfo/question/faq-how-do-i-increase-sexual-arousal" class="more-button">Read More</a></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="module-footer">
+    <a href="<?php print $base_path . "question" ?>">All Questions &raquo;</a>
+  </div>
+</div>
 
 
-  <!-- Right box
-  ========================-->
-  <div class="column-third">
-    <!-- BOX TITLE -->
-    <h4 class="module-title">Ask the Sexperts</h4>
-    <div class="module ask-module">
-      <div class="node-box">
-        <div class="node-content">
-          <p>Can't find an answer to your question? Want to send in feedback on the site? We'd love to hear from you! Click <a href="<?php print $base_path . "ask-sexperts" ?>">here</a> to send a question to the Sexperts.</p>
-        </div><!--.node-content-->
-        <div class="node-footer">
-          <a href="<?php print $base_path . "ask-sexperts" ?>">Ask a Question &raquo;</a>
-        </div><!--.node-footer-->
-      </div><!--.node-box-->
-    </div><!-- .module -->
-  </div><!-- .column-third -->
 
-</div><!-- .box-module-container -->
+<!-- Ask Sexperts -->
+<div class="span4 front-module">
+  <h4 class="module-title">Ask the Sexperts</h4>
+
+  <div class="module-content ask-module">
+    <p>Can't find an answer to your question? Want to send in feedback on the site? We'd love to hear from you!
+      Click <a href="<?php print $base_path . "ask-sexperts" ?>">here</a> to send a question to the Sexperts.</p>
+  </div>
+
+  <div class="module-footer">
+    <a href="<?php print $base_path . "ask-sexperts" ?>">Ask a Question &raquo;</a>
+  </div>
+</div>
