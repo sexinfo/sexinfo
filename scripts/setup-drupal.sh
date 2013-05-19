206cc78bcf596b47eeea338d9dd8185c284d50c5
Mark Nguyen <mpnguyen@umail.ucsb.edu>
2013-05-18 19:01:38 -0700

    Initial commit of importing databases and setting up drupal and setting up databases scripts

---
+++ scripts/setup-drupal.sh
@@ -0,0 +1,2 @@
+curl 'http://localhost/sexinfo/install.php' -H 'Cookie: Drupal.toolbar.collapsed=0; SESS1b26a221601aaf499712a3ca2bf68263=X4svHPej_FXSP9r1YKU5X9mV6Sb_OPRA97K2Mlu-tq8; has_js=1' --data 'profile=standard&form_build_id=form-EF9B5wVJGYFwxOs87PJ94WRrAmaaxZ4ZVcWN10P59fM&form_id=install_select_profile_form&op=Save+and+continue' --compressed
+
