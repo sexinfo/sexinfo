6b958ada58a054edd763e5382597d2f7621c4e43
John Tran <johntran@resnet-13-48.resnet.ucsb.edu>
2013-05-20 13:57:47 -0700

    Changed Search Results 404

--- modules/search/search-results.tpl.php
+++ modules/search/search-results.tpl.php
@@ -28,6 +28,12 @@
   </ol>
   <?php print $pager; ?>
 <?php else : ?>
-  <h2><?php print t('Your search yielded no results');?></h2>
-  <?php print search_help('search#noresults', drupal_help_arg()); ?>
+   <div class="content clearfix"><div id="search404-result-text"><br><center><p><font size = "5", font color = "#F52887"> Your search yielded no results. <br> Try another search above or send us a question or comment.</font></center>
+   	<p><font size = "3", font color = "#000000"><ul>
+<li>Check if your spelling is correct.</li>
+<li>Remove quotes around phrases to search for each word individually. 
+	<ul><li><em>bike shed</em> will often show more results than <em>&quot;bike shed&quot;</em>.</li></ul>
+<li>Consider loosening your query with <em>OR</em>. <em>bike OR shed</em> will often show more results than <em>bike shed</em>.</li>
+</ul></font>
+<center><p style ="padding:6px;color:pink;background-color:black;border:black 2px solid"font size = "5", font color = "#F52887"><a href = "/sexinfo/ask-sexperts">Click here to send us a question or comment.</a></font></p></center></div>
 <?php endif; ?>
