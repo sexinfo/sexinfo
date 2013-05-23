0d91cb7b2b945f00aae75df83373efb88ce8f9c7
Mark Nguyen <mpnguyen@umail.ucsb.edu>
2013-05-23 16:13:00 -0700

    interum state of affairs

--- scripts/setup-drupal.sh
+++ scripts/setup-drupal.sh
@@ -1,2 +1,7 @@
-curl 'http://localhost/sexinfo/install.php' -H 'Cookie: Drupal.toolbar.collapsed=0; SESS1b26a221601aaf499712a3ca2bf68263=X4svHPej_FXSP9r1YKU5X9mV6Sb_OPRA97K2Mlu-tq8; has_js=1' --data 'profile=standard&form_build_id=form-EF9B5wVJGYFwxOs87PJ94WRrAmaaxZ4ZVcWN10P59fM&form_id=install_select_profile_form&op=Save+and+continue' --compressed
-
+index_HTML=$(curl -c cookies.txt "http://localhost/sexinfo/install.php")
+form_build_id=$(echo "$index_HTML" | grep form_build_id | cut -d'"' -f 6)
+form_build_id=$(curl -L -b cookies.txt "http://localhost/sexinfo/install.php" --data "profile=standard&form_build_id=$form_build_id&form_id=install_select_profile_form&op=Save and continue" | grep form_build_id | cut -d'"' -f 6)
+form_build_id=$(echo "$index_HTML" | grep form_build_id | cut -d'"' -f 6)
+form_build_id=$(curl -L -b cookies.txt "http://localhost/sexinfo/install.php?profile=standard" --data "locale=en&form_build_id=$form_build_id&form_id=install_select_locale_form&op=Save and continue")
+form_build_id=$(curl -L -b cookies.txt "http://localhost/sexinfo/install.php?profile=standard&locale=en" --data "locale=en&form_build_id=$form_build_id&form_id=install_select_locale_form&op=Save and continue")
+echo "$form_build_id"
