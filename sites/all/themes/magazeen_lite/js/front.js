59f28d6d4a85a9c90c6f1ba7e422c1a341bb1e9e
Andrew Berls <andrew.berls@gmail.com>
2013-05-25 19:59:21 -0700

    Move front page javascript out to separate file and refactor a bit

---
+++ sites/all/themes/magazeen_lite/js/front.js
@@ -0,0 +1,72 @@
+$(function() {
+
+  /*----------------------------
+    Image carousel module
+  ----------------------------*/
+  var i = 1,
+      loopRotate   = true, // Automatic cycling is disabled once user clicks on a carousel link
+      numLinks     = 8,
+      imageHeight  = 250,
+      $navLinks    = $('#carousel-nav a'),
+      currentClass = 'current';
+
+
+  // Scroll the reel
+  function rotate(id) {
+    var offset = Math.abs(id-1),
+      distance = imageHeight * offset;
+
+    $('#window-inner').animate({
+      top: -distance
+    }, 550);
+  }
+
+
+  // Rotate the image frame and update links
+  function cycle() {
+    rotate(i);
+    $navLinks.removeClass(currentClass);
+
+    $navLinks.each(function(idx, link) {
+      if (parseInt($(link).data("num")) === i)
+        $(link).addClass(currentClass);
+    });
+
+    // Increment the current frame and reset to first if at end
+    i++;
+    if (i == (numLinks+1)) {
+      i = 1;
+    }
+  }
+
+
+  // Slide to selected link and disable auto cycling afterwards
+  $navLinks.click(function() {
+    clearInterval(loop);
+    $navLinks.removeClass(currentClass);
+    $(this).addClass(currentClass);
+    rotate(parseInt($(this).data('num')));
+    return false;
+  });
+
+
+  if (loopRotate) {
+    // Cycle the frame every 5 seconds if the user hasn't clicked a link
+    var loop = setInterval(cycle, 5000);
+  }
+
+
+
+  /*----------------------------
+    Sliding FAQ captions
+  ----------------------------*/
+  $(".caption-slide").hover(
+    function () {
+      $(this).animate({ bottom: '0' }, 300);     // Slide up
+    },
+    function () {
+      $(this).animate({ bottom: '-40px' }, 300); // Slide down
+    }
+  );
+
+});
--- sites/all/themes/magazeen_lite/templates/page--front.tpl.php
+++ sites/all/themes/magazeen_lite/templates/page--front.tpl.php
@@ -11,98 +11,19 @@
   ?>
 
   <?php print render($page['help']); ?>
-
 </div>
+
 
 <div class="carousel-container">
   <?php include 'modules/carousel.php' ?>
 </div>
 
 
-
 <div class="container front-container">
-
   <div class="row">
     <?php include 'modules/box_grid.php' ?>
   </div>
-
 </div>
 
 
 <?php include 'footer.php' ?>
-
-
-
-<script type="text/javascript">
-  (function ($) {
-
-    /*----------------------------
-      Image carousel module
-    ----------------------------*/
-    var loopRotate   = true, // Automatic cycling is disabled once user clicks on a carousel link
-        numLinks     = 8,
-        image_height = 250,
-        $navLinks    = $('#carousel-nav a');
-
-    function rotate(id) {
-      var offset = Math.abs(id-1),
-        distance = image_height * offset;
-
-      $('#window-inner').animate({
-        top: -distance
-      }, 550);
-    }
-
-    var i = 1;
-
-    function cycle() {
-      // Rotate the image frame
-      rotate(i);
-      $navLinks.removeClass('current');
-
-      // Loop through all links in nav list
-      // If the link's rel attribute (used as an identifier) matches current frame, add class 'current'
-      $navLinks.each(function(idx, link) {
-        if ( $(link).attr("rel") == i ) {
-          $(link).addClass("current");
-        }
-      });
-
-      // Increment the current frame and reset to first if at end
-      i++;
-      if (i == (numLinks+1)) {
-        i = 1;
-      }
-    }
-
-
-    $navLinks.click(function() {
-      clearInterval(loop);              // Disable auto cycle on click
-      $navLinks.removeClass('current'); // Remove current class from all tabs
-      $(this).addClass('current');
-      rotate( $(this).attr('rel') );
-      return false;                     // Prevent browser jump to anchor link
-    });
-
-
-    if (loopRotate) {
-      // Cycle the frame every 5 seconds if the user hasn't clicked a link
-      var loop = setInterval(cycle, 5000);
-    }
-
-
-
-    /*----------------------------
-      Sliding FAQ captions
-    ----------------------------*/
-    $(".caption-slide").hover(
-      function () {
-        $(this).animate({ bottom: '0' }, 300);     // Slide up
-      },
-      function () {
-        $(this).animate({ bottom: '-40px' }, 300); // Slide down
-      }
-    );
-
-  })(jQuery);
-</script>
