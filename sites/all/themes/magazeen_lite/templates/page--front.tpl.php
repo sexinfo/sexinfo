f8e1c42477a8c2324eabb419649d7034dd0b22b5
Mark Nguyen <mpnguyen@umail.ucsb.edu>
2013-06-04 19:17:29 -0700

    Linked list items for recent changes

--- sites/all/themes/magazeen_lite/templates/page--front.tpl.php
+++ sites/all/themes/magazeen_lite/templates/page--front.tpl.php
@@ -4,11 +4,18 @@
 
 <h3>Recent Changes:</h3>
 <ul>
-<?php $query = db_select('node', 'n')->fields('n', array('title'))->range(0, 10)->orderBy('changed', 'DESC');
-      $result = $query->execute()->fetchCol();
-      foreach($result as $item) {
-        echo '<li>'; echo $item; echo '</li>';
-      } ?>
+<?php $query = db_select('node', 'n')->fields('n', array('nid'))->range(0, 10)->orderBy('changed', 'DESC');
+
+      $nodeIDs = $query->execute()->fetchCol();
+      $type = 'node';
+      $conditions = array();
+      $resetCache = FALSE;
+
+      $nodes = entity_load($type, $nodeIDs, $conditions, $resetCache);
+      foreach($nodes as $node) {
+        echo '<li><a href="node/'; echo $node->nid; echo '">'; echo $node->title; echo '</a></li>';
+      }
+      ?>
 </ul>
 <div class="container">
   <?php if ($breadcrumb): print $breadcrumb; endif; ?>
