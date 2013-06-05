c95c83ac84066f57a3d6c5588ce9baaea8bfa1b6
rnkaufman <rnkaufman@umail.ucsb.edu>
2013-06-05 11:01:44 -0700

    Fixed a <div> tag in the quizzes page

--- sites/all/themes/magazeen_lite/templates/node.tpl.php
+++ sites/all/themes/magazeen_lite/templates/node.tpl.php
@@ -1,3 +1,4 @@
+<div>
   <div id="node-<?php print $node->nid; ?>" class="node<?php if ($sticky) { print ' sticky'; } ?><?php if (!$status) { print ' node-unpublished'; } ?>">
 
     <?php print $user_picture; ?>
