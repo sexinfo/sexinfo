d5a772a398d0836952b5faa1e55b53ca2bcbbde1
Andrew Berls <andrew.berls@gmail.com>
2013-10-17 12:09:26 -0700

    Style dropdown for demo

--- sites/all/themes/magazeen_lite/templates/navigation.php
+++ sites/all/themes/magazeen_lite/templates/navigation.php
@@ -1,60 +1,40 @@
   <div id="navigation">
     <div class="container clearfix">
 
-      <ul id="main-menu" class="links main-menu"><li class="menu-339 active-trail first active"><a href="/home" title="" class="active-trail active">Home</a></li>
-<li class="menu-537"><a href="/category">Topics</a>
-	<div id "topics-menu">
+      <ul id="main-menu" class="links main-menu">
+        <li class="menu-339 active-trail first active"><a href="/sexinfo/home" title="" class="active-trail active">Home</a></li>
 
-		<ul>
-      <li><a href="sub1.html" >Basics of Sexuality</a></li>
-      <li><a href="sub2.html" >The Body</a></li>
-      <li><a href="sub3.html" >Beliefs and Sexuality</a></li>
-      <li><a href="sub4.html" >Contraception</a></li>
-      <li><a href="sub5.html" >Abortion</a></li>
-      <li><a href="sub6.html" >Health</a></li>
-      <li><a href="sub6.html" >Love & Relationships</a></li>
-      <li><a href="sub6.html" >Pregnancy</a></li>
-      <li><a href="sub6.html" >Sex and the Law</a></li>
-      <li><a href="sub6.html" >Sexual Activity</a></li>
-      <li><a href="sub6.html" >Sexual Difficulties</a></li>
-      <li><a href="sub6.html" >Sexual Orientations</a></li>
-      <li><a href="sub6.html" >Sexually Transmitted Infections</a></li>
-    </ul>
+        <li class="menu-537">
+          <a href="/sexinfo/category">Topics</a>
+          <div id="topics-menu">
+            <ul>
+              <div class="dropdown-half">
+                <li><a href="#" >Basics of Sexuality</a></li>
+                <li><a href="#" >The Body</a></li>
+                <li><a href="#" >Beliefs and Sexuality</a></li>
+                <li><a href="#" >Contraception</a></li>
+                <li><a href="#" >Abortion</a></li>
+                <li><a href="#" >Health</a></li>
+                <li><a href="#" >Love &amp; Relationships</a></li>
+              </div>
 
+              <div class="dropdown-half">
+                <li><a href="#" >Pregnancy</a></li>
+                <li><a href="#" >Sex and the Law</a></li>
+                <li><a href="#" >Sexual Activity</a></li>
+                <li><a href="#" >Sexual Difficulties</a></li>
+                <li><a href="#" >Sexual Orientations</a></li>
+                <li><a href="#" >Sexually Transmitted Infections</a></li>
+              </div>
 
-  <script type="text/javascript" src="js/jquery.js"></script>
-  <script type="text/javascript">
-$(document).ready(function () { 
+            </ul>
+          </div><!-- .topics-menu -->
+        </li>
 
-  var $menu = $('.menu-537 div');
-  
-  $('.menu-537').hover(
-    function () {
-      //show its submenu
-      $menu.show();
-
-    }, 
-    function () {
-      //hide its submenu
-      $menu.hide();   
-    }
-  );
-  
-});
-  </script>
-
-
-
-
-
-
-	</div>	
-</li>
-
-<li class="menu-549"><a href="/frequently-asked-questions">FAQs</a></li>
-<li class="menu-400"><a href="/ask-sexperts">Ask the Sexperts</a></li>
-<li class="menu-543"><a href="/article/resources">Resources</a></li>
-<li class="menu-739 last"><a href="/quizzes">Test Your Knowledge</a></li>
-</ul>
+        <li class="menu-549"><a href="/sexinfo/frequently-asked-questions">FAQs</a></li>
+        <li class="menu-400"><a href="/sexinfo/ask-sexperts">Ask the Sexperts</a></li>
+        <li class="menu-543"><a href="/sexinfo/article/resources">Resources</a></li>
+        <li class="menu-739 last"><a href="/sexinfo/quizzes">Test Your Knowledge</a></li>
+      </ul><!-- #main-menu -->
     </div>
-  </div>
+  </div><!-- #navigation -->
