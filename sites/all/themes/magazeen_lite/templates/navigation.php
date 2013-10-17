2c912448f24792992438eaa2d3bc4a07811ced7d
Andrew Pang <andrewpang94@gmail.com>
2013-10-16 21:48:19 -0700

    Fixed merge conflicts

--- sites/all/themes/magazeen_lite/templates/navigation.php
+++ sites/all/themes/magazeen_lite/templates/navigation.php
@@ -1,15 +1,3 @@
-<!-- 
-<?php if (!empty($main_menu)): ?>
-  <div id="navigation">
-    <div class="container clearfix">
-
-      <?php print theme('links', array('links' => $main_menu, 'attributes' => array('id' => 'main-menu', 'class' => array('links', 'main-menu')))); ?>
-
-    </div>
-  </div><!-- #navigation --   (>)
-<?php endif;?>
--->
-
   <div id="navigation">
     <div class="container clearfix">
 
