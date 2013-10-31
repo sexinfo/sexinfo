cbc651bbed3c5d9eb07e2b6555b7124f71e19b89
Andrew Berls <andrew.berls@gmail.com>
2013-10-31 16:14:12 -0700

    Fix Resources link in navigation

--- sites/all/themes/magazeen_lite/templates/navigation.php
+++ sites/all/themes/magazeen_lite/templates/navigation.php
@@ -33,9 +33,11 @@
 
         <li class="menu-549"><a href="/sexinfo/frequently-asked-questions">FAQs</a></li>
         <li class="menu-400"><a href="/sexinfo/ask-sexperts">Ask the Sexperts</a></li>
-        <li class="menu-543"><a href="/sexinfo/article/resources">Resources</a></li>
-        <?php $result = db_query("SELECT * FROM {node} WHERE status=1 AND promote=1 ORDER BY RAND() LIMIT 1");
-          $randomNode = $result->fetch() ?>
+        <li class="menu-543"><a href="/sexinfo/article/important-phone-numbersresources">Resources</a></li>
+        <?php
+          $result     = db_query("select * from {node} where status = 1 and promote = 1 order by rand() limit 1");
+          $randomnode = $result->fetch()
+        ?>
         <li class="menu-543"><a href="/sexinfo/node/<?php echo $randomNode->nid;?>" title="<?php echo $randomNode->title?>">Random Article</a></li>
         <li class="menu-739 last"><a href="/sexinfo/quizzes">Test Your Knowledge</a></li>
       </ul><!-- #main-menu -->
