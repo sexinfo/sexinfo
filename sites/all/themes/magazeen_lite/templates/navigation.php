cf574eabc42b1756dc3b720719ff6123b294293f
Andrew Berls <andrew.berls@gmail.com>
2013-10-31 16:16:14 -0700

    Fix capitalization issue

--- sites/all/themes/magazeen_lite/templates/navigation.php
+++ sites/all/themes/magazeen_lite/templates/navigation.php
@@ -36,7 +36,7 @@
         <li class="menu-543"><a href="/sexinfo/article/important-phone-numbersresources">Resources</a></li>
         <?php
           $result     = db_query("select * from {node} where status = 1 and promote = 1 order by rand() limit 1");
-          $randomnode = $result->fetch()
+          $randomNode = $result->fetch()
         ?>
         <li class="menu-543"><a href="/sexinfo/node/<?php echo $randomNode->nid;?>" title="<?php echo $randomNode->title?>">Random Article</a></li>
         <li class="menu-739 last"><a href="/sexinfo/quizzes">Test Your Knowledge</a></li>
