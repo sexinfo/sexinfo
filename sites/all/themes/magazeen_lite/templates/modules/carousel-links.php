9b454d743f326aa92db09beec909cc1870e4e076
Mark Nguyen <mpnguyen@umail.ucsb.edu>
2013-11-05 23:34:06 -0800

    Included image as clickable for carousel links on right hand side

--- sites/all/themes/magazeen_lite/templates/modules/carousel-links.php
+++ sites/all/themes/magazeen_lite/templates/modules/carousel-links.php
@@ -9,8 +9,8 @@ $phpArray = json_decode($jsonData, true);
   <div class="carousel-nav-left">
   <?php for ($i = 0; $i < 4; $i++) { $linkItem = $phpArray[$i];?>
     <li>
-      <img class="carousel-thumb" src="<?php print path_to_theme() . '/images/modules/' . $linkItem['image']; ?>" />
       <a class="current" data-num="<?php echo $i+1; ?>" href="#">
+      <img class="carousel-thumb" src="<?php print path_to_theme() . '/images/modules/' . $linkItem['image']; ?>" />
         <?php echo $linkItem['title']; ?>
       </a>
     </li>
@@ -20,8 +20,8 @@ $phpArray = json_decode($jsonData, true);
   <div class="carousel-nav-right">
   <?php for ($i = 4; $i < count($phpArray); $i++) { $linkItem = $phpArray[$i];?>
     <li>
-      <img class="carousel-thumb" src="<?php print path_to_theme() . '/images/modules/' . $linkItem['image']; ?>" />
       <a class="current" data-num="<?php echo $i+1; ?>" href="#">
+      <img class="carousel-thumb" src="<?php print path_to_theme() . '/images/modules/' . $linkItem['image']; ?>" />
         <?php echo $linkItem['title']; ?>
       </a>
     </li>
