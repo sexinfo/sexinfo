cac521301a775a50b07fe59fdd887e3e1ba5ae28
Julio Garcia <juliogarcia93@gmail.com>
2013-11-06 17:27:01 -0800

    Removing test code.

--- sites/all/themes/magazeen_lite/js/lib/autocomplete.js
+++ sites/all/themes/magazeen_lite/js/lib/autocomplete.js
@@ -2,11 +2,8 @@ $( document ).ready(function() {
 	$countries = $('#edit-submitted-location');
 	$countries.prop("autocomplete", "off");
 	$countries.addClass("typeahead");
-	
 
-	var colors = ["red", "blue", "green", "yellow", "brown", "black"];
-
-	  $countries.typeahead({      
+	$countries.typeahead({      
 	  name: 'countries',                                                                                   
 	  prefetch: '/data/countries.json',                                         
 	  limit: 10                                                                   
