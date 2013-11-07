e723716850ee0f11de6a0c377378c724e24331cc
Julio Garcia <juliogarcia93@gmail.com>
2013-11-06 17:25:49 -0800

    Fixing duplicate calls to JQuery by putting the Id into a variable.

--- sites/all/themes/magazeen_lite/js/lib/autocomplete.js
+++ sites/all/themes/magazeen_lite/js/lib/autocomplete.js
@@ -1,11 +1,12 @@
 $( document ).ready(function() {
-	$('#edit-submitted-location').prop("autocomplete", "off");
-	$('#edit-submitted-location').addClass("typeahead");
+	$countries = $('#edit-submitted-location');
+	$countries.prop("autocomplete", "off");
+	$countries.addClass("typeahead");
 	
 
-var colors = ["red", "blue", "green", "yellow", "brown", "black"];
+	var colors = ["red", "blue", "green", "yellow", "brown", "black"];
 
-	  $('#edit-submitted-location').typeahead({      
+	  $countries.typeahead({      
 	  name: 'countries',                                                                                   
 	  prefetch: '/data/countries.json',                                         
 	  limit: 10                                                                   
