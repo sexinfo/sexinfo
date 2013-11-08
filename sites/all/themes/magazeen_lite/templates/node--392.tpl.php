b89cdb981f52479c314ce90fb428a88da381c16e
Mark Nguyen <mpnguyen@umail.ucsb.edu>
2013-11-07 18:49:13 -0800

    Reswapped node--392.tpl.php with backup for production

--- sites/all/themes/magazeen_lite/templates/node--392.tpl.php
+++ sites/all/themes/magazeen_lite/templates/node--392.tpl.php
@@ -1,88 +1,39 @@
-<div class="topics-container">
-
+<div class="container">
+  <h1 class="topic-header">SexInfo Topics</h1>
+  
   <?php
-    # TODO: these are temporary demo placeholders
-    $imgBench  = 'http://31.media.tumblr.com/03e99a662f35e2755ce1ae54c9b32223/tumblr_mufrp1cpsZ1st5lhmo1_1280.jpg';
-    $imgBoat   = 'http://25.media.tumblr.com/b03e53931e2264f97a3e93beae6d3053/tumblr_mtw7hple1a1st5lhmo1_1280.jpg';
-    $imgDrops  = 'http://25.media.tumblr.com/737df9d56207ad851080aea91343ddfe/tumblr_mtw7tgyBKP1st5lhmo1_1280.jpg';
-    $imgSpace  = 'http://31.media.tumblr.com/234548bb90cc22d64ff5dcd5a3d73e40/tumblr_mtw7pktaK11st5lhmo1_1280.jpg';
-    $imgForest = 'http://24.media.tumblr.com/78a064d4f7036848b9bb3661a91b6fdc/tumblr_mufr0mmWYW1st5lhmo1_1280.jpg';
-    $imgBridge = 'http://31.media.tumblr.com/31bdd576c16543588f4af868dc31de7f/tumblr_mtw81dbwCN1st5lhmo1_1280.jpg';
-    $imgHouse  = 'http://25.media.tumblr.com/7e8f8b9961fb51966ac341f16a3290f6/tumblr_mtaw41DfOy1st5lhmo1_1280.jpg';
+  // The ID of the taxonomy vocabulary for which you'd like to create a nested list
+  $vid = 3;
+  $depth = 0;
+  $tree = taxonomy_get_tree($vid);
+  $counter = 0;
+
+  print '<ul class="taxonomy-parent">';
+
+  foreach ($tree as $term) {
+    if ($term->depth > $depth) {
+      print '<ul class="taxonomy-nested">';
+      $depth = $term->depth;
+    }
+    if ($term->depth < $depth) {
+      print '</ul>';
+      $depth = $term->depth;
+    }
+
+    // Color for every other list item
+    if ($counter % 2 == 0) $className = "item-even";
+    else $className = "item-odd";
+
+    if ($term->depth == 0) {
+      print "<li class='taxonomy-item " . $className . "' style='font-weight: bold;'>" . $term->name . "</li>";
+    } else {
+      print "<li class='taxonomy-item " . $className . "'>" . l($term->name, "taxonomy/term/" . 
+        $term->tid . "/all") . "</li>";
+    }
+    
+    $counter++;
+  }
+
+  print '</ul>';
   ?>
-
-  <div class="parent-topic">
-
-    <div class="grid-left">
-      <div class="topic-half">
-          <div class="text-on-image" style="background: url('<?php echo $imgBoat ?>') no-repeat">
-              <div class="text-on-image-text">Basics of Sexuality</div>
-            </div>
-      </div>
-    </div>
-
-    <div class="grid-right">
-      <div class="topic-quarter">
-            <div class="text-on-image" style="background: url('<?php echo $imgDrops ?>') no-repeat">
-              <div class="text-on-image-text">Anatomy</div>
-            </div>
-      </div>
-      <div class="topic-quarter">
-            <div class="text-on-image" style="background: url('<?php echo $imgForest ?>') no-repeat">
-              <div class="text-on-image-text">Sexual Activity</div>
-            </div>
-      </div>
-    </div>
-
-  </div><!-- .parent-topic -->
-
-
-
-  <div class="parent-topic">
-
-    <div class="grid-left">
-      <div class="topic-half">
-            <div class="text-on-image" style="background: url('<?php echo $imgBench ?>') no-repeat">
-              <div class="text-on-image-text">Pregnancy</div>
-            </div>
-      </div>
-    </div>
-
-    <div class="grid-right">
-      <div class="topic-half">
-            <div class="text-on-image" style="background: url('<?php echo $imgBridge ?>') no-repeat">
-              <div class="text-on-image-text">Contraception</div>
-            </div>
-      </div>
-    </div>
-
-  </div><!-- .parent-topic -->
-
-
-
-  <div class="parent-topic">
-
-    <div class="grid-left">
-      <div class="topic-quarter">
-            <div class="text-on-image" style="background: url('<?php echo $imgHouse ?>') no-repeat">
-              <div class="text-on-image-text">Abortion</div>
-            </div>
-      </div>
-      <div class="topic-quarter">
-            <div class="text-on-image" style="background: url('<?php echo $imgDrops ?>') no-repeat">
-              <div class="text-on-image-text">STI's</div>
-            </div>
-      </div>
-    </div>
-
-    <div class="grid-right">
-      <div class="topic-half">
-            <div class="text-on-image" style="background: url('<?php echo $imgSpace ?>') no-repeat">
-              <div class="text-on-image-text">Health</div>
-            </div>
-      </div>
-    </div>
-
-  </div><!-- .parent-topic -->
-
-</div><!-- .topics-container -->
+</div>
