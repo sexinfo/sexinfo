dd662bf5782e4efbd136807631817280f840f8af
Mark Nguyen <mpnguyen@umail.ucsb.edu>
2013-11-24 13:17:47 -0800

    Fixed upload-databases.sh to use DropboxUploader with colorized error output and helpful hints to downloading Dropbox Uploader.

--- scripts/upload-databases.sh
+++ scripts/upload-databases.sh
@@ -1,15 +1,17 @@
-#! /bin/sh
+#! /bin/bash
+
+if [ ! -d "./Dropbox-Uploader/" ] || [ ! -f "./Dropbox-Uploader/dropbox_uploader.sh" ]; then
+  echo -e "\e[0;31mError: Could not find dropbox_uploader.sh.\e[0m\nPlease run '\e[1;33mgit submodule update\e[0m' from your root sexinfo folder."
+  exit 1;
+fi
 
 rootDBFolder="/Shared Webmaster/Database Exports/"
 
 uploadDirectory="$rootDBFolder`date +'%Y'`/`date +'%m.%d.%y.%H%M'`/"
 
-#./Dropbox-Uploader/dropbox_uploader.sh mkdir "$uploadDirectory"
-./dropbox_uploader.sh mkdir "$uploadDirectory"
-
+./Dropbox-Uploader/dropbox_uploader.sh mkdir "$uploadDirectory"
 
 for arg in "$@"
 do
-	#./Dropbox-Uploader/dropbox_uploader.sh upload "$arg" "${uploadDirectory}$arg"
-	./dropbox_uploader.sh upload "$arg" "${uploadDirectory}$arg"
+	./Dropbox-Uploader/dropbox_uploader.sh upload "$arg" "${uploadDirectory}$arg"
 done
