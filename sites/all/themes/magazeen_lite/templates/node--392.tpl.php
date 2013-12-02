a865f043c676325a9a1df75c0a4d48d8c4339bd0
Andrew Berls <andrew.berls@gmail.com>
2013-12-01 20:34:43 -0800

    First implementation of topic loading from JSON
    WIP
    
    Coding the new topics page by hand is infeasible, given that we have
    many sections each containing many articles. We'll want to automatically
    generate HTML by reading some configuration file - all topic info is
    placed in /data/topics.json. This is read once and used for generating
    the page display.
    
    Note: the code reading the topics file is currently a massive hack. The
    `global` keyword wasn't working, preventing us from doing:
    
      $topics = getTopics();
      function renderTopics() {
        global $topics;
        // etc...
      }
    
    Instead, we're forced to use a dirty hack with the superglobal $GLOBALS
    variable.
    
    /data/topics.json is currently a skeleton - it contains entries for
    article categories (which equate to the links on the current topics
    page), but the individual articles within each section aren't added. We
    may want to write a script or something to manually extract article
    names/urls as it will be an extremely time intensive process otherwise.

--- sites/all/themes/magazeen_lite/templates/node--392.tpl.php
+++ sites/all/themes/magazeen_lite/templates/node--392.tpl.php
@@ -1,192 +1,86 @@
-	<div class="topics-container">
+<?php include 'utils/content_tag.php' ?>
 
-  <?php
-    # TODO: these are temporary demo placeholders
-    $imgBench  = 'http://31.media.tumblr.com/03e99a662f35e2755ce1ae54c9b32223/tumblr_mufrp1cpsZ1st5lhmo1_1280.jpg';
-    $imgBoat   = 'http://25.media.tumblr.com/b03e53931e2264f97a3e93beae6d3053/tumblr_mtw7hple1a1st5lhmo1_1280.jpg';
-    $imgDrops  = 'http://25.media.tumblr.com/737df9d56207ad851080aea91343ddfe/tumblr_mtw7tgyBKP1st5lhmo1_1280.jpg';
-    $imgSpace  = 'http://31.media.tumblr.com/234548bb90cc22d64ff5dcd5a3d73e40/tumblr_mtw7pktaK11st5lhmo1_1280.jpg';
-    $imgForest = 'http://24.media.tumblr.com/78a064d4f7036848b9bb3661a91b6fdc/tumblr_mufr0mmWYW1st5lhmo1_1280.jpg';
-    $imgBridge = 'http://31.media.tumblr.com/31bdd576c16543588f4af868dc31de7f/tumblr_mtw81dbwCN1st5lhmo1_1280.jpg';
-    $imgHouse  = 'http://25.media.tumblr.com/7e8f8b9961fb51966ac341f16a3290f6/tumblr_mtaw41DfOy1st5lhmo1_1280.jpg';
-  ?>
+<?php
+  // Decode /data/topics.json as associative array
+  function getTopics() {
+    $fileData = file_get_contents("data/topics.json", true);
+    return json_decode($fileData, true);
+  }
+
+  // TODO: remove this
+  $GLOBALS['topics'] = getTopics();
+
+  // Render a half-width topic box for a specified topic name
+  //
+  //  $size - The size of the section, 'half' or 'quarter'
+  //  $topicName - Name of the topic JSON section, e.g. 'The Sexual Response Cycle'
+  //
+  // The heredoc can't be indented!
+  // See http://php.net/manual/en/language.types.string.php#language.types.string.syntax.heredoc
+  function renderTopicSection($size, $topicName) {
+    $topics = $GLOBALS['topics'];
+
+    $topic  = $topics[$topicName];
+    $image  = $topic['image'];
+
+    // Pre-construct children list items to substitute into heredoc string
+    // Ex: <li class="text-on-image-article"><a href="http://...">Sex Around The World</a></li>
+    $children = "";
+    foreach($topic['children'] as $name => $url) {
+      $link = content_tag("a", $name, array("href" => $url));
+      $children .= content_tag('li', $link, array('class' => 'text-on-image-article'));
+    }
+
+return <<<HTML
+<div class="topic-{$size}">
+  <div class="text-on-image" style="background: url('$image') no-repeat">
+    <div class="text-on-image-tint">
+    <div class="text-on-image-text">{$topicName}</div>
+      <ul class="text-on-image-articles">{$children}</ul>
+    </div>
+  </div>
+</div>
+HTML;
+  }
+
+  function renderTopicHalf($topicName) {
+    return renderTopicSection('half', $topicName);
+  }
+
+  function renderTopicQuarter($topicName) {
+    return renderTopicSection('quarter', $topicName);
+  }
+
+?>
+
+<div class="topics-container">
 
   <div class="parent-topic">
     <h2>Basics of Sexuality</h2>
+
     <div class="grid-left">
-      <div class="topic-quarter">
-        <div class="text-on-image" style="background: url('<?php echo $imgBench ?>') no-repeat">
-          <div class="text-on-image-tint">
-            <div class="text-on-image-text">An Overview of Sexuality</div>
-            <ul class="text-on-image-articles">
-              <li class="text-on-image-article"><a href="#">An Overview of Sexuality</a></li>
-            </ul><!-- .text-on-image-articles -->
-          </div>
-        </div>
-      </div>
-      <div class="topic-quarter">
-        <div class="text-on-image" style="background: url('<?php echo $imgBoat ?>') no-repeat"><div class="text-on-image-tint">
-          <div class="text-on-image-text">Puberty</div>
-            <ul class="text-on-image-articles">
-              <li class="text-on-image-article"><a href="#">Male Puberty</a></li>
-              <li class="text-on-image-article"><a href="#">Nocturnal Orgasms and Emissions</a></li>
-              <li class="text-on-image-article"><a href="#">Female Puberty</a></li>
-              <li class="text-on-image-article"><a href="#">Getting Your Period</a></li>
-            </ul><!-- .text-on-image-articles -->
-          </div>
-        </div>
-      </div>
+      <?php echo renderTopicHalf('Sex In The Life Cycle') ?>
     </div>
 
     <div class="grid-right">
-      <div class="topic-quarter">
-        <div class="text-on-image" style="background: url('<?php echo $imgDrops ?>') no-repeat">
-          <div class="text-on-image-tint">
-            <div class="text-on-image-text">Aging and the Sexual Response</div>
-            <ul class="text-on-image-articles">
-              <li class="text-on-image-article"><a href="#">Aging and the Sexual Response</a></li>
-            </ul><!-- .text-on-image-articles -->
-          </div>
-        </div>
-      </div>
-      <div class="topic-quarter">
-        <div class="text-on-image" style="background: url('<?php echo $imgForest ?>') no-repeat">
-          <div class="text-on-image-tint">
-            <div class="text-on-image-text">Sexual Activity</div>
-            <ul class="text-on-image-articles">
-              <li class="text-on-image-article"><a href="#">Sex Education within Schools</a></li>
-            </ul><!-- .text-on-image-articles -->
-          </div>
-        </div>
-      </div>
+      <?php echo renderTopicHalf('Talking About Sex') ?>
     </div>
   </div><!-- .parent-topic -->
 
 
 
   <div class="parent-topic">
-    <h2>Anatomy</h2>
+    <h2>The Body</h2>
+
     <div class="grid-left">
-      <div class="topic-quarter">
-            <div class="text-on-image" style="background: url('<?php echo $imgBench ?>') no-repeat"><div class="text-on-image-tint">
-              <div class="text-on-image-text">Male Reproductive System</div>
-              <ul class="text-on-image-articles">
-                <li class="text-on-image-article"><a href="#">Overview of the Male Reproductive System</a></li>
-                <li class="text-on-image-article"><a href="#">The Penis</a></li>
-                <li class="text-on-image-article"><a href="#">The Testes</a></li>
-                <li class="text-on-image-article"><a href="#">The Prostate Gland</a></li>
-              </ul><!-- .text-on-image-articles -->
-            </div>
-      </div></div>
-      <div class="topic-quarter">
-            <div class="text-on-image" style="background: url('<?php echo $imgBench ?>') no-repeat"><div class="text-on-image-tint">
-              <div class="text-on-image-text">Female Reproductive System</div>
-              <ul class="text-on-image-articles">
-                <li class="text-on-image-article"><a href="#">Overview of the Female Reproductive System</a></li>
-                <li class="text-on-image-article"><a href="#">Egg Facts</a></li>
-                <li class="text-on-image-article"><a href="#">The Breasts</a></li>
-                <li class="text-on-image-article"><a href="#">The G-Spot</a></li>
-                <li class="text-on-image-article"><a href="#">The Clitoris</a></li>
-                <li class="text-on-image-article"><a href="#">The Hymen</a></li>
-                <li class="text-on-image-article"><a href="#">The Menstrual Cycle</a></li>
-              </ul><!-- .text-on-image-articles -->
-            </div>
-      </div></div>
+      <?php echo renderTopicQuarter('Sex Determination') ?>
+      <?php echo renderTopicQuarter('Sexual Response') ?>
     </div>
 
     <div class="grid-right">
-      <div class="topic-quarter">
-            <div class="text-on-image" style="background: url('<?php echo $imgBridge ?>') no-repeat"><div class="text-on-image-tint">
-              <div class="text-on-image-text">Sexual Response</div>
-              <ul class="text-on-image-articles">
-                <li class="text-on-image-article"><a href="#">The Sexual Response Cycle</a></li>
-                <li class="text-on-image-article"><a href="#">Epididymal Hypertension (Blue Balls)</a></li>
-                <li class="text-on-image-article"><a href="#">Pelvic Congestion</a></li>
-              </ul><!-- .text-on-image-articles -->
-            </div></div>
-      </div>
-      <div class="topic-quarter">
-            <div class="text-on-image" style="background: url('<?php echo $imgBridge ?>') no-repeat"><div class="text-on-image-tint">
-              <div class="text-on-image-text">Sex Determination</div>
-              <ul class="text-on-image-articles">
-                <li class="text-on-image-article"><a href="#">Sex Determination and Differentiation</a></li>
-                <li class="text-on-image-article"><a href="#">Intersex</a></li>
-              </ul><!-- .text-on-image-articles -->
-            </div>
-      </div></div>
+      <?php echo renderTopicQuarter('Female Body') ?>
+      <?php echo renderTopicQuarter('Male Body') ?>
     </div>
-
-  </div><!-- .parent-topic -->
-
-
-
-  <div class="parent-topic">
-    <h2>Sexual Activity</h2>
-    <div class="grid-left">
-      <div class="topic-quarter">
-            <div class="text-on-image" style="background: url('<?php echo $imgHouse ?>') no-repeat"><div class="text-on-image-tint">
-              <div class="text-on-image-text">Masturbation</div>
-              <ul class="text-on-image-articles">
-                <li class="text-on-image-article"><a href="#">Female Masturbation</a></li>
-                <li class="text-on-image-article"><a href="#">Male Masturbation</a></li>
-                <li class="text-on-image-article"><a href="#">Fantaasy and Erogenous Zones</a></li>
-                <li class="text-on-image-article"><a href="#">A Guide to Sex Toys</a></li>
-              </ul><!-- .text-on-image-articles -->
-            </div></div>
-      </div>
-      <div class="topic-quarter">
-            <div class="text-on-image" style="background: url('<?php echo $imgDrops ?>') no-repeat"><div class="text-on-image-tint">
-              <div class="text-on-image-text">Sex with Others</div>
-              <ul class="text-on-image-articles">
-                <li class="text-on-image-article"><a href="#">Manual Stimulation</a></li>
-                <li class="text-on-image-article"><a href="#">Penile/Vaginal Sex</a></li>
-                <li class="text-on-image-article"><a href="#">Article 1</a></li>
-                <li class="text-on-image-article"><a href="#">Article 1</a></li>
-                <li class="text-on-image-article"><a href="#">Article 1</a></li>
-                <li class="text-on-image-article"><a href="#">Article 1</a></li>
-                <li class="text-on-image-article"><a href="#">Manual Stimulation</a></li>
-                <li class="text-on-image-article"><a href="#">Penile/ Vaginal Sex (Intercourse)</a></li>
-                <li class="text-on-image-article"><a href="#">Oral Sex</a></li>
-                <li class="text-on-image-article"><a href="#">Deep-Throating During Oral Sex</a></li>
-                <li class="text-on-image-article"><a href="#">Using Barriers During Oral Sex</a></li>
-                <li class="text-on-image-article"><a href="#">Dental Dams</a></li>
-                <li class="text-on-image-article"><a href="#">Swallowing Your Partner’s Ejaculation</a></li>
-                <li class="text-on-image-article"><a href="#">Dry Sex</a></li>
-                <li class="text-on-image-article"><a href="#">Anal Sex</a></li>
-                <li class="text-on-image-article"><a href="#">Group Sex</a></li>
-                <li class="text-on-image-article"><a href="#">Swinging</a></li>
-                <li class="text-on-image-article"><a href="#">Sexual Positions for Overweight Individuals and Pregnant Couples</a></li>
-                <li class="text-on-image-article"><a href="#">Sex on Your Period</a></li>
-                <li class="text-on-image-article"><a href="#">Queefing Demystified</a></li>
-              </ul><!-- .text-on-image-articles -->
-            </div>
-      </div></div>
-    </div>
-
-    <div class="grid-right">
-      <div class="topic-half">
-            <div class="text-on-image" style="background: url('<?php echo $imgSpace ?>') no-repeat"><div class="text-on-image-tint">
-              <div class="text-on-image-text">Sex with Others</div>
-              <ul class="text-on-image-articles">
-                <li class="text-on-image-article"><a href="#">Manual Stimulation</a></li>
-                <li class="text-on-image-article"><a href="#">Penile/Vaginal Sex (Intercourse)</a></li>
-                <li class="text-on-image-article"><a href="#">Oral Sex</a></li>
-                <li class="text-on-image-article"><a href="#">Deep-Throating During Oral Sex</a></li>
-                <li class="text-on-image-article"><a href="#">Using Barriers During Oral Sex</a></li>
-                <li class="text-on-image-article"><a href="#">Dental Dams</a></li>
-                <li class="text-on-image-article"><a href="#">Swallowing Your Partner's Ejaculation</a></li>
-                <li class="text-on-image-article"><a href="#">Dry Sex</a></li>
-                <li class="text-on-image-article"><a href="#">Anal Sex</a></li>
-                <li class="text-on-image-article"><a href="#">Group Sex</a></li>
-                <li class="text-on-image-article"><a href="#">Swinging</a></li>
-                <li class="text-on-image-article"><a href="#">Sexual Positions for Overweight Individuals and Pregnant Couples</a></li>
-                <li class="text-on-image-article"><a href="#">Sex on Your Period</a></li>
-                <li class="text-on-image-article"><a href="#">Queefing Demystified</a></li>
-              </ul><!-- .text-on-image-articles -->
-            </div>
-      </div></div>
-    </div>
-
   </div><!-- .parent-topic -->
 
 </div><!-- .topics-container -->
