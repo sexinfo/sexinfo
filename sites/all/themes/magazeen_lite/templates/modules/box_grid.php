980eaf7150c5b1e006f3ce067b217f8b2041b926
Andrew Berls <andrew.berls@gmail.com>
2014-01-28 21:13:50 -0800

    Change up front page boxes
    This adds the Open Sans font globally  (which is scientifically proven
    to make you at least 45% hipper), and re-styles the boxes on the front
    page to look more awesome.

--- sites/all/themes/magazeen_lite/templates/modules/box_grid.php
+++ sites/all/themes/magazeen_lite/templates/modules/box_grid.php
@@ -1,3 +1,44 @@
+<div class="third front-module">
+  <h3>Frequently Asked Questions</h3>
+  <!--<p>Lorem ipsum dolor sit amet, lorem ipsum.</p>-->
+
+  <div class="module-content">
+    <div class="faq-image">
+      <a href="http://www.soc.ucsb.edu/sexinfo/question/faq-how-do-i-increase-sexual-arousal">
+      <div style="background: url('<?php print path_to_theme() . '/images/modules/' . 'arousal.jpg'; ?>')" class="faq-image-box"></div>
+
+      <div class="caption-slide">
+        <h3>How Do I Increase Sexual Arousal?</h3></a>
+        <p>There are many ways to increase your and your partner’s sexual arousal. Sexual arousal is... <a href="http://www.soc.ucsb.edu/sexinfo/question/faq-how-do-i-increase-sexual-arousal">Read More &gt;</a>
+        </p>
+      </div>
+    </div>
+  </div>
+
+</div>
+
+
+<div class="third front-module">
+  <h3>Explore our Topics</h3>
+  <p style="font-size: 1.4em; line-height: 1.3;">
+    We've completely redone our topics page to make it easier to find what you're looking for.
+    <a href="#">Take a look &raquo;</a>
+  </p>
+</div>
+
+
+<div class="third front-module">
+  <h3>Test Your Knowledge</h3>
+  <p>Do you think you know it all?</p>
+  <p>Are you an expert on masturbation, LGBTQ facts, paraphilias, or pregnancy and abortion?</p>
+  <p>Quiz yourself <a href="http://www.soc.ucsb.edu/sexinfo/quizzes">here</a> to see if you really are.</p>
+
+  <a class="pink-block-btn" href="#">Take the quiz</a>
+</div>
+
+
+<?php
+/*
 <!-- Frequently Asked Questions -->
 <div class="span4 front-module">
   <h4 class="module-title">Frequently Asked Questions</h4>
@@ -42,4 +83,7 @@ For COMMERCIAL USE LICENSING, visit http://tagcrowd.com/licensing.html
   <div class="module-footer">
     <a href="<?php print $base_path . "quizzes" ?>">Take a quiz &raquo;</a>
   </div>
-</div>
+  </div>
+
+ */
+?>
--- sites/all/themes/magazeen_lite/templates/page--front.tpl.php
+++ sites/all/themes/magazeen_lite/templates/page--front.tpl.php
@@ -2,6 +2,7 @@
 
 <?php include 'navigation.php' ?>
 
+
 <div class="container">
   <?php if ($breadcrumb): print $breadcrumb; endif; ?>
 
@@ -11,18 +12,16 @@
   ?>
 
   <?php print render($page['help']); ?>
-</div>
 
-
-<div class="carousel-container">
-  <?php include 'modules/carousel.php' ?>
-</div>
-
-
-<div class="container front-container">
-  <div class="row">
-    <?php include 'modules/box_grid.php' ?>
+  <div class="carousel-container">
+    <?php include 'modules/carousel.php' ?>
   </div>
+
+
+  <div class="front-container">
+      <?php include 'modules/box_grid.php' ?>
+  </div>
+
 </div>
 
 
