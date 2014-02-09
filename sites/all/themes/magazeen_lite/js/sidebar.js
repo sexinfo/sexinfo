25c9f3b3c2087e3fc03835da60a862905e966bd3
Andrew Berls <andrew.berls@gmail.com>
2014-02-08 20:39:09 -0800

    Add scrollspy and smooth scroll to topics page
    This adds a slightly modified version of Scrollspy from bootstrap to add
    scroll tracking to the topics nav. Smooth scrolling on click is
    implemented as well.

--- sites/all/themes/magazeen_lite/js/sidebar.js
+++
@@ -1,9 +0,0 @@
-	$(function(){
-		$(".suggestions-container").html(
-        h2("Suggested Articles") + ul(function(){
-          return li("First article") + li ("Second article") + li ("Third article") + li ("Wow, it never ends!");
-        })
-
-      );
-	});
-
