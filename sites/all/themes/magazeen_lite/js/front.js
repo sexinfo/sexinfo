c32c9ca00c3dadadf052e7cff3f03c1698734909
Andrew Pang <andrewpang94@gmail.com>
2014-02-18 20:03:04 -0800

    Prototype for the new carousel

--- sites/all/themes/magazeen_lite/js/front.js
+++ sites/all/themes/magazeen_lite/js/front.js
@@ -1,72 +1,205 @@
-$(function() {
-
-  /*----------------------------
-    Image carousel module
-  ----------------------------*/
-  var i = 1,
-      loopRotate   = true, // Automatic cycling is disabled once user clicks on a carousel link
-      numLinks     = 8,
-      imageHeight  = 250,
-      $navLinks    = $('#carousel-nav a'),
-      currentClass = 'current';
+/* ========================================================================
+ * Bootstrap: carousel.js v3.1.1
+ * http://getbootstrap.com/javascript/#carousel
+ * ========================================================================
+ * Copyright 2011-2014 Twitter, Inc.
+ * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
+ * ======================================================================== */
 
 
-  // Scroll the reel
-  function rotate(id) {
-    var offset = Math.abs(id-1),
-      distance = imageHeight * offset;
++function ($) {
+  'use strict';
 
-    $('#window-inner').animate({
-      top: -distance
-    }, 550);
+  // CAROUSEL CLASS DEFINITION
+  // =========================
+
+  var Carousel = function (element, options) {
+    this.$element    = $(element)
+    this.$indicators = this.$element.find('.carousel-indicators')
+    this.options     = options
+    this.paused      =
+    this.sliding     =
+    this.interval    =
+    this.$active     =
+    this.$items      = null
+
+    this.options.pause == 'hover' && this.$element
+      .on('mouseenter', $.proxy(this.pause, this))
+      .on('mouseleave', $.proxy(this.cycle, this))
   }
 
+  Carousel.DEFAULTS = {
+    interval: 5000,
+    pause: 'hover',
+    wrap: true
+  }
 
-  // Rotate the image frame and update links
-  function cycle() {
-    rotate(i);
-    $navLinks.removeClass(currentClass);
+  Carousel.prototype.cycle =  function (e) {
+    e || (this.paused = false)
 
-    $navLinks.each(function(idx, link) {
-      if (parseInt($(link).data("num")) === i)
-        $(link).addClass(currentClass);
-    });
+    this.interval && clearInterval(this.interval)
 
-    // Increment the current frame and reset to first if at end
-    i++;
-    if (i == (numLinks+1)) {
-      i = 1;
+    this.options.interval
+      && !this.paused
+      && (this.interval = setInterval($.proxy(this.next, this), this.options.interval))
+
+    return this
+  }
+
+  Carousel.prototype.getActiveIndex = function () {
+    this.$active = this.$element.find('.item.active')
+    this.$items  = this.$active.parent().children()
+
+    return this.$items.index(this.$active)
+  }
+
+  Carousel.prototype.to = function (pos) {
+    var that        = this
+    var activeIndex = this.getActiveIndex()
+
+    if (pos > (this.$items.length - 1) || pos < 0) return
+
+    if (this.sliding)       return this.$element.one('slid.bs.carousel', function () { that.to(pos) })
+    if (activeIndex == pos) return this.pause().cycle()
+
+    return this.slide(pos > activeIndex ? 'next' : 'prev', $(this.$items[pos]))
+  }
+
+  Carousel.prototype.pause = function (e) {
+    e || (this.paused = true)
+
+    if (this.$element.find('.next, .prev').length && $.support.transition) {
+      this.$element.trigger($.support.transition.end)
+      this.cycle(true)
     }
+
+    this.interval = clearInterval(this.interval)
+
+    return this
   }
 
-
-  // Slide to selected link and disable auto cycling afterwards
-  $navLinks.click(function() {
-    clearInterval(loop);
-    $navLinks.removeClass(currentClass);
-    $(this).addClass(currentClass);
-    rotate(parseInt($(this).data('num')));
-    return false;
-  });
-
-
-  if (loopRotate) {
-    // Cycle the frame every 5 seconds if the user hasn't clicked a link
-    var loop = setInterval(cycle, 5000);
+  Carousel.prototype.next = function () {
+    if (this.sliding) return
+    return this.slide('next')
   }
 
+  Carousel.prototype.prev = function () {
+    if (this.sliding) return
+    return this.slide('prev')
+  }
 
+  Carousel.prototype.slide = function (type, next) {
+    var $active   = this.$element.find('.item.active')
+    var $next     = next || $active[type]()
+    var isCycling = this.interval
+    var direction = type == 'next' ? 'left' : 'right'
+    var fallback  = type == 'next' ? 'first' : 'last'
+    var that      = this
 
-  /*----------------------------
-    Sliding FAQ captions
-  ----------------------------*/
-  $(".faq-image").hover(
-    function () {
-      $(".caption-slide").animate({ bottom: '0' }, 300);     // Slide up
-    },
-    function () {
-      $(".caption-slide").animate({ bottom: '-40px' }, 300); // Slide down
+    if (!$next.length) {
+      if (!this.options.wrap) return
+      $next = this.$element.find('.item')[fallback]()
     }
-  );
 
-});
+    if ($next.hasClass('active')) return this.sliding = false
+
+    var e = $.Event('slide.bs.carousel', { relatedTarget: $next[0], direction: direction })
+    this.$element.trigger(e)
+    if (e.isDefaultPrevented()) return
+
+    this.sliding = true
+
+    isCycling && this.pause()
+
+    if (this.$indicators.length) {
+      this.$indicators.find('.active').removeClass('active')
+      this.$element.one('slid.bs.carousel', function () {
+        var $nextIndicator = $(that.$indicators.children()[that.getActiveIndex()])
+        $nextIndicator && $nextIndicator.addClass('active')
+      })
+    }
+
+    if ($.support.transition && this.$element.hasClass('slide')) {
+      $next.addClass(type)
+      $next[0].offsetWidth // force reflow
+      $active.addClass(direction)
+      $next.addClass(direction)
+      $active
+        .one($.support.transition.end, function () {
+          $next.removeClass([type, direction].join(' ')).addClass('active')
+          $active.removeClass(['active', direction].join(' '))
+          that.sliding = false
+          setTimeout(function () { that.$element.trigger('slid.bs.carousel') }, 0)
+        })
+        .emulateTransitionEnd($active.css('transition-duration').slice(0, -1) * 1000)
+    } else {
+      $active.removeClass('active')
+      $next.addClass('active')
+      this.sliding = false
+      this.$element.trigger('slid.bs.carousel')
+    }
+
+    isCycling && this.cycle()
+
+    return this
+  }
+
+
+  // CAROUSEL PLUGIN DEFINITION
+  // ==========================
+
+  var old = $.fn.carousel
+
+  $.fn.carousel = function (option) {
+    return this.each(function () {
+      var $this   = $(this)
+      var data    = $this.data('bs.carousel')
+      var options = $.extend({}, Carousel.DEFAULTS, $this.data(), typeof option == 'object' && option)
+      var action  = typeof option == 'string' ? option : options.slide
+
+      if (!data) $this.data('bs.carousel', (data = new Carousel(this, options)))
+      if (typeof option == 'number') data.to(option)
+      else if (action) data[action]()
+      else if (options.interval) data.pause().cycle()
+    })
+  }
+
+  $.fn.carousel.Constructor = Carousel
+
+
+  // CAROUSEL NO CONFLICT
+  // ====================
+
+  $.fn.carousel.noConflict = function () {
+    $.fn.carousel = old
+    return this
+  }
+
+
+  // CAROUSEL DATA-API
+  // =================
+
+  $(document).on('click.bs.carousel.data-api', '[data-slide], [data-slide-to]', function (e) {
+    var $this   = $(this), href
+    var $target = $($this.attr('data-target') || (href = $this.attr('href')) && href.replace(/.*(?=#[^\s]+$)/, '')) //strip for ie7
+    var options = $.extend({}, $target.data(), $this.data())
+    var slideIndex = $this.attr('data-slide-to')
+    if (slideIndex) options.interval = false
+
+    $target.carousel(options)
+
+    if (slideIndex = $this.attr('data-slide-to')) {
+      $target.data('bs.carousel').to(slideIndex)
+    }
+
+    e.preventDefault()
+  })
+
+  $(window).on('load', function () {
+    $('[data-ride="carousel"]').each(function () {
+      var $carousel = $(this)
+      $carousel.carousel($carousel.data())
+    })
+  })
+
+}(jQuery);
--- sites/all/themes/magazeen_lite/templates/page--front.tpl.php
+++ sites/all/themes/magazeen_lite/templates/page--front.tpl.php
@@ -4,20 +4,56 @@
 
 
 <div class="container">
-  <?php if ($breadcrumb): print $breadcrumb; endif; ?>
+  <div id="carousel-example-generic" class="carousel slide">
 
-  <?php
-    # "has been successfully updated", etc
-    if ($show_messages && $messages): print $messages; endif;
-  ?>
 
-  <?php print render($page['help']); ?>
+<!-- Wrapper for slides -->
+    <div class="carousel-inner">
 
-  <div class="carousel-container">
-    <?php include 'modules/carousel.php' ?>
+      <div class="item active">
+        <img src="http://www.soc.ucsb.edu/sexinfo/sites/all/themes/magazeen_lite/images/modules/pregnant.jpg" alt="">
+        <div class="carousel-caption">
+
+         <div class="container">
+           <h3>Can I Get Pregnant If...?</h3><p>Becoming a mother is a very important and amazing transition in life. Bringing a new life into the world brings…</p></div>
+           <p><a class="btn btn-primary btn-lg" role="button">Read more</a></p>
+
+        </div>
+      </div>
+
+      <div class="item">
+        <img src="http://localhost:8888/sites/all/themes/magazeen_lite/images/modules/arousal.jpg" alt="">
+        <div class="carousel-caption">
+        
+          <div class="container">
+          <h3>Sexual Behaviors</h3><p>Sexual techniques are methods for increasing a partner's sexual arousal. These include activites that…</p></div>
+          <p><a class="btn btn-primary btn-lg" role="button">Read more</a></p>
+          
+        </div>
+      </div>
+
+      <div class="item">
+        <img src="http://localhost:8888/sites/all/themes/magazeen_lite/images/modules/arousal.jpg" alt="">
+        <div class="carousel-caption">
+          
+          <div class = "container">
+          <h3>Can I Get Pregnant If...?</h3><p>Becoming a mother is a very important and amazing transition in life. Bringing a new life into the world brings…</p></div>
+          <p><a class="btn btn-primary btn-lg" role="button">Read more</a></p>
+        
+        </div>
+      </div>
+    </div>
+    
+      <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left"></span>
+      </a>
+      <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right"></span>
+      </a>
   </div>
+</div>
 
-
+<div class = "container">
   <div class="front-container cloud-container">
     <?php include 'modules/tag_cloud.php' ?>
   </div>
@@ -28,5 +64,6 @@
   </div>
 
 </div><!-- .container -->
+
 
 <?php include 'footer.php' ?>
