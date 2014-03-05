fdc0e81d588afe4f77efcd5aadba03201e2ae375
Andrew Berls <andrew.berls@gmail.com>
2014-03-04 21:06:11 -0800

    Link fix to use /sexinfo document root

--- sites/all/themes/magazeen_lite/templates/page--front.tpl.php
+++ sites/all/themes/magazeen_lite/templates/page--front.tpl.php
@@ -11,7 +11,7 @@
     <div class="carousel-inner">
 
       <div class="item active">
-        <img src="/sites/all/themes/magazeen_lite/images/modules/sexOnPeriod.png" alt="">
+        <img src="/sexinfo/sites/all/themes/magazeen_lite/images/modules/sexOnPeriod.png" alt="">
         <div class="carousel-caption">
 
          <div class="carouselContainer">
@@ -22,28 +22,28 @@
       </div>
 
       <div class="item">
-        <img src="/sites/all/themes/magazeen_lite/images/modules/ejaculate.png" alt="">
+        <img src="/sexinfo/sites/all/themes/magazeen_lite/images/modules/ejaculate.png" alt="">
         <div class="carousel-caption">
-        
+
           <div class="carouselContainer">
           <h3>Swallowing Your Partner's Ejaculate</h3><p>First off, the notion that semen has the ability to whiten teeth is no more than a myth…</p></div>
           <p><a class="btn btn-primary btn-lg" href = "http://www.soc.ucsb.edu/sexinfo/article/swallowing-your-partners-ejaculate" role="button" >Read more</a></p>
-          
+
         </div>
       </div>
 
       <div class="item">
-        <img src="/sites/all/themes/magazeen_lite/images/modules/hymen.png" alt="">
+        <img src="/sexinfo/sites/all/themes/magazeen_lite/images/modules/hymen.png" alt="">
         <div class="carousel-caption">
-          
+
           <div class = "carouselContainer">
           <h3>Is My Hymen Intact?</h3><p>If you are curious about what your hymen (or what is left of your hymen) looks like…</p></div>
           <p><a class="btn btn-primary btn-lg" href = "http://www.soc.ucsb.edu/sexinfo/question/my-hymen-intact" role="button">Read more</a></p>
-        
+
         </div>
       </div>
     </div>
-    
+
       <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
         <span class="glyphicon glyphicon-chevron-left"></span>
       </a>
