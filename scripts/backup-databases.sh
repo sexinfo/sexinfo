02eef515f79a1d1fc9eaf3c9f0e899403efe265b
Mark Nguyen <mpnguyen@umail.ucsb.edu>
2014-03-04 20:21:04 -0800

    Hard coded username and mans out on no input detected

--- scripts/backup-databases.sh
+++ scripts/backup-databases.sh
@@ -1,7 +1,14 @@
 #! /bin/bash
 
+# Sanity check
+if [ $# -eq 0 ]
+then
+  echo -e "\033[0;31mNo Password Supplied\033[0m"
+  exit
+fi
+
 # Download the databases
-./download-databases.sh $1 $2
+./download-databases.sh "sexweb00m" $1
 
 # Only upload the databases to dropbox if the download was successful
 if [[ $? -eq 0 ]]; then
