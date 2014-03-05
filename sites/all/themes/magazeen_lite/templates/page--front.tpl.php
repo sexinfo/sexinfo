6d16fd2b49f39eef0de6609b636d82297649e4c9
Andrew Pang <andrewpang94@gmail.com>
2014-03-04 20:33:30 -0800

    Updated carousel on the front page

--- sites/all/themes/magazeen_lite/templates/page--front.tpl.php
+++ sites/all/themes/magazeen_lite/templates/page--front.tpl.php
@@ -4,41 +4,41 @@
 
 
 <div class="container">
-  <div id="carousel-example-generic" class="carousel slide">
+  <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
 
 
 <!-- Wrapper for slides -->
     <div class="carousel-inner">
 
       <div class="item active">
-        <img src="http://www.soc.ucsb.edu/sexinfo/sites/all/themes/magazeen_lite/images/modules/pregnant.jpg" alt="">
+        <img src="http://localhost:8888/sites/all/themes/magazeen_lite/images/modules/sexOnPeriod.png" alt="">
         <div class="carousel-caption">
 
-         <div class="container">
-           <h3>Can I Get Pregnant If...?</h3><p>Becoming a mother is a very important and amazing transition in life. Bringing a new life into the world brings…</p></div>
-           <p><a class="btn btn-primary btn-lg" role="button">Read more</a></p>
+         <div class="carouselContainer">
+           <h3>Sex on Your Period</h3><p>It is common for people to avoid having sexual intercourse during menstruation…</p></div>
+           <p><a class="btn btn-primary btn-lg" href = "http://www.soc.ucsb.edu/sexinfo/article/sex-your-period" role="button">Read more</a></p>
 
         </div>
       </div>
 
       <div class="item">
-        <img src="http://localhost:8888/sites/all/themes/magazeen_lite/images/modules/arousal.jpg" alt="">
+        <img src="http://localhost:8888/sites/all/themes/magazeen_lite/images/modules/ejaculate.png" alt="">
         <div class="carousel-caption">
         
-          <div class="container">
-          <h3>Sexual Behaviors</h3><p>Sexual techniques are methods for increasing a partner's sexual arousal. These include activites that…</p></div>
-          <p><a class="btn btn-primary btn-lg" role="button">Read more</a></p>
+          <div class="carouselContainer">
+          <h3>Swallowing Your Partner's Ejaculate</h3><p>First off, the notion that semen has the ability to whiten teeth is no more than a myth…</p></div>
+          <p><a class="btn btn-primary btn-lg" href = "http://www.soc.ucsb.edu/sexinfo/article/swallowing-your-partners-ejaculate" role="button" >Read more</a></p>
           
         </div>
       </div>
 
       <div class="item">
-        <img src="http://localhost:8888/sites/all/themes/magazeen_lite/images/modules/arousal.jpg" alt="">
+        <img src="http://localhost:8888/sites/all/themes/magazeen_lite/images/modules/hymen.png" alt="">
         <div class="carousel-caption">
           
-          <div class = "container">
-          <h3>Can I Get Pregnant If...?</h3><p>Becoming a mother is a very important and amazing transition in life. Bringing a new life into the world brings…</p></div>
-          <p><a class="btn btn-primary btn-lg" role="button">Read more</a></p>
+          <div class = "carouselContainer">
+          <h3>Is My Hymen Intact?</h3><p>If you are curious about what your hymen (or what is left of your hymen) looks like…</p></div>
+          <p><a class="btn btn-primary btn-lg" href = "http://www.soc.ucsb.edu/sexinfo/question/my-hymen-intact" role="button">Read more</a></p>
         
         </div>
       </div>
