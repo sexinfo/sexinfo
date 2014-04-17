b3ffbaa65ca526cc2cae416dccfc01e9b7fdc10f
Mark Nguyen <mpnguyen@umail.ucsb.edu>
2014-04-16 22:54:20 -0700

    Defaulted options menu to fancy autocomplete with no autocomplete and good style

--- sites/all/themes/magazeen_lite/js/lib/autocomplete.js
+++ sites/all/themes/magazeen_lite/js/lib/autocomplete.js
@@ -1,12 +1,14 @@
 $( document ).ready(function() {
+
+  $('select').selectToAutocomplete();
 	$countries = $('#edit-submitted-location');
 	$countries.prop("autocomplete", "off");
 	$countries.addClass("typeahead");
 
-	$countries.typeahead({      
-	  name: 'countries',                                                                                   
-	  prefetch: '/data/countries.json',                                         
-	  limit: 10                                                                   
+	$countries.typeahead({
+	  name: 'countries',
+	  prefetch: '/data/countries.json',
+	  limit: 10
 	});
 
 });
