d2a5b5d1095d250c04fb8ad69b8bf4120f94e995
Mark Nguyen <mpnguyen@umail.ucsb.edu>
2014-05-08 17:17:58 -0700

    We are committing to SexInfo Online

--- sites/all/themes/magazeen_lite/templates/footer.php
+++ sites/all/themes/magazeen_lite/templates/footer.php
@@ -2,8 +2,8 @@
   <div class="container clearfix">
 
 	  	<div class="left">
-		   	<h4>About SexInfoOnline</h4>
-		    <p><?print '<a href="'. check_url($front_page) .'">';?>SexInfoOnline</a> is a website devoted to comprehensive sex education based on the best research we have to date. Our primary goal is to ensure that people around the world have access to useful and accurate information about all aspects of human sexuality.</p>
+		   	<h4>About SexInfo Online</h4>
+		    <p><?print '<a href="'. check_url($front_page) .'">';?>SexInfo Online</a> is a website devoted to comprehensive sex education based on the best research we have to date. Our primary goal is to ensure that people around the world have access to useful and accurate information about all aspects of human sexuality.</p>
 		    <p>The site is maintained by university students from the University of California, Santa Barbara who have studied advanced topics in human sexuality.</p>
 		</div>
 
