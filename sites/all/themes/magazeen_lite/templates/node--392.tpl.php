f1e0fa70c4887664f8d25c918cc1821f5766a2d6
Mark Nguyen <mpnguyen@umail.ucsb.edu>
2014-05-15 20:28:06 -0700

    Restyled and refactored some code generation

--- sites/all/themes/magazeen_lite/templates/node--392.tpl.php
+++ sites/all/themes/magazeen_lite/templates/node--392.tpl.php
@@ -68,46 +68,55 @@ HTML;
       }
 
       // Return generated HTML
-      return $result;
+      return $result . "</ul>";
+  }
+
+  function renderTopics() {
+      // Query DB for top level topics
+      $topics = db_query('SELECT * FROM taxonomy_term_data A, taxonomy_term_hierarchy B WHERE A.tid=B.tid AND A.vid=3 AND B.parent=0 ORDER BY weight ASC');
+
+      $result = "<div class=\"topics-container\">";
+
+      // For each topic, make a link in the list
+      foreach($topics as $topic) {
+        $result = $result . renderTopic($topic);
+      }
+
+      // Return generated HTML
+      return $result . "</div>";
+  }
+
+  function renderTopic($topic) {
+    $name = $topic->name;
+    $target = urlencode($name);
+    $result = <<<HTML
+<div class="parent-topic" id="$target">
+  <h2>$name</h2>
+
+  <div class="grid-left">
+    <?php echo renderTopicHalf('Adolescent Sexuality') ?>
+  </div>
+
+  <div class="grid-right">
+    <?php echo renderTopicQuarter('Aging and the Sexual Response') ?>
+    <?php echo renderTopicQuarter('Talking About Sex') ?>
+  </div>
+</div><!-- .parent-topic -->
+HTML;
   }
 
   echo renderNav();
+  echo renderTopics();
   ?>
 
-<ul class="topics-nav">
-  <div class="header-container">
-    <h4>Explore Our Topics!</h4>
-  </div>
-
-  <li><a href="#basics_of_sexuality">Sex Across The Lifecycle</a></li>
-  <li><a href="#the_body">The Body</a></li>
-  <li><a href="#sexual_activity">Sexual Activity</a></li>
-  <li><a href="#pregnancy">Pregnancy</a></li>
-  <li><a href="#contraception">Birth Control</a></li>
-  <li><a href="#abortion">Abortion</a></li>
-  <li><a href="#sexually_transmitted_infections">Sexually Transmitted Infections (STIs)</a></li>
-  <li><a href="#health">Health</a></li>
-  <li><a href="#love_and_relationships">Love and Relationships</a></li>
-  <li><a href="#sexual_orientations">Sexual Orientations</a></li>
-  <li><a href="#sexual_difficulties">Sexual Difficulties</a></li>
-  <li><a href="#sex_and_the_law">Sex and the Law</a></li>
-  <li><a href="#beliefs_and_sexuality">Beliefs and Sexuality</a></li>
-</ul>
-
-
-<div class="topics-container">
 
   <div class="parent-topic" id="basics_of_sexuality">
     <h2>Sex Across the Lifecycle</h2>
 
-    <div class="grid-left">
       <?php echo renderTopicHalf('Adolescent Sexuality') ?>
-    </div>
 
-    <div class="grid-right">
       <?php echo renderTopicQuarter('Aging and the Sexual Response') ?>
       <?php echo renderTopicQuarter('Talking About Sex') ?>
-    </div>
   </div><!-- .parent-topic -->
 
 </div><!-- .topics-container -->
