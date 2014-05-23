78049c95f756b8673b72ace43a794ba7b2ff6311
Mark Nguyen <mpnguyen@umail.ucsb.edu>
2014-05-22 19:00:39 -0700

    Only download countries if we're on the right page

--- sites/all/themes/magazeen_lite/js/lib/autocomplete.js
+++ sites/all/themes/magazeen_lite/js/lib/autocomplete.js
@@ -1,8 +1,10 @@
 $( document ).ready(function() {
   // If javascript is enabled, replace the standard webform select stuff with our cool country selection!
-  $.get('data/countries.html', function( data ) {
-    $('#edit-submitted-location').replaceWith( data ); // Replace the data!
-    $('#edit-submitted-location').selectToAutocomplete(); // Enables our autocomplete mode
-    $('.tt-hint').remove(); // Removes the hint that blocks our select item from being seein
-  });
+  if ($('#edit-submitted-location').length) {
+    $.get('data/countries.html', function( data ) {
+      $('#edit-submitted-location').replaceWith( data ); // Replace the data!
+      $('#edit-submitted-location').selectToAutocomplete(); // Enables our autocomplete mode
+      $('.tt-hint').remove(); // Removes the hint that blocks our select item from being seein
+    });
+  }
 });
