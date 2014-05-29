2329738af488753940df1070c69f05de83dbbad8
Andrew Berls <andrew.berls@gmail.com>
2014-05-29 12:48:07 -0700

    Add Constitution link to footer

--- sites/all/themes/magazeen_lite/templates/footer.php
+++ sites/all/themes/magazeen_lite/templates/footer.php
@@ -1,20 +1,28 @@
 <div id="footer">
   <div class="container clearfix">
 
-	  	<div class="left">
-		   	<h4>About SexInfo Online</h4>
-		    <p><?print '<a href="'. check_url($front_page) .'">';?>SexInfo Online</a> is a website devoted to comprehensive sex education based on the best research we have to date. Our primary goal is to ensure that people around the world have access to useful and accurate information about all aspects of human sexuality.</p>
-		    <p>The site is maintained by university students from the University of California, Santa Barbara who have studied advanced topics in human sexuality.</p>
-		</div>
+    <div class="left">
+       <h4>About SexInfo Online</h4>
+      <p>
+        <a href="<?php print check_url($front_page) ?>">SexInfo Online</a> is a website devoted to
+        comprehensive sex education based on the best research we have to date. Our primary goal is to
+        ensure that people around the world have access to useful and accurate information about all aspects of human sexuality.
+      </p>
 
-		<div class="right">
-			<h4>Elsewhere on the Web</h4>
-			<ul class="social">
-				<a href="https://www.facebook.com/SexInfoOnline"><li><i class="icon-large icon-facebook-sign"></i>Be a Fan on Facebook</li></a>
-				<a href="https://twitter.com/sexinfoonline"><li><i class="icon-large icon-twitter-sign"></i>Follow Us on Twitter</li></a>
-				<a href="http://www.youtube.com/user/UCSBSexInfoOnline"><li><i class="icon-large icon-facetime-video"></i>Visit Our Youtube Channel</li></a>
-				<a href="https://github.com/sexinfo/sexinfo"><li><i class="icon-large icon-github-alt"></i>We're Open Source!</li></a>
-			</ul>
+      <p>
+        The site is maintained by students from the University of California, Santa Barbara who have studied advanced topics in human sexuality.
+        You can read our mission statement <a href="<?php print $base_path . "docs/constitution.pdf" ?>">here</a>.
+      </p>
+    </div>
+
+    <div class="right">
+      <h4>Elsewhere on the Web</h4>
+      <ul class="social">
+        <a href="https://www.facebook.com/SexInfoOnline"><li><i class="icon-large icon-facebook-sign"></i>Be a Fan on Facebook</li></a>
+        <a href="https://twitter.com/sexinfoonline"><li><i class="icon-large icon-twitter-sign"></i>Follow Us on Twitter</li></a>
+        <a href="http://www.youtube.com/user/UCSBSexInfoOnline"><li><i class="icon-large icon-facetime-video"></i>Visit Our Youtube Channel</li></a>
+        <a href="https://github.com/sexinfo/sexinfo"><li><i class="icon-large icon-github-alt"></i>We're Open Source!</li></a>
+      </ul>
     </div>
 
   </div>
@@ -24,6 +32,6 @@
 <div id="snavigation">
   <div class="container clearfix">
   <p>UCSB SexInfo Copyright &copy; 2013 University of California, Santa Barbara. All Rights Reserved.</p>
-	<a href="<?php print $base_path . "login" ?>" class="login">Writer Login &raquo;</a>
+  <a href="<?php print $base_path . "login" ?>" class="login">Writer Login &raquo;</a>
   </div>
 </div> <!-- #snavigation -->
