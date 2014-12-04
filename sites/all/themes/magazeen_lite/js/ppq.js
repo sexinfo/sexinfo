2edf640254b8cef90d1654b600b0bc5e4240e46d
Mark Nguyen <mpnguyen@umail.ucsb.edu>
2014-12-04 15:06:35 -0800

    Added Julio's initial PPQ code to our repo -- Not quite working yet but not quite crashing

---
+++ sites/all/themes/magazeen_lite/js/ppq.js
@@ -0,0 +1,93 @@
+﻿$quiz = $('.quiz-body')
+$question = $('#question');
+$answers = $('#answers ul');
+$response = $('.response');
+var questions;
+var responses;
+
+$(document).ready(function () {
+
+    questions = loadJSON("questions.json");
+    responses = loadJSON("responses.json");
+    console.log(questions);
+    console.log(responses);
+
+    var startKey = questions['start'];
+    var currentQuestion = questions[startKey];
+
+    $('#TermsOfService').click(function () {
+        if ($(this).is(':checked') == true) {
+            $('#start-quiz').attr('disabled', true);
+        } else
+        {
+            $('#start-quiz').attr('disabled', false);
+        }
+    });
+    
+    $('#start-quiz').click(function () {
+        $question.html(currentQuestion.message);
+        $(this).fadeOut('slow', function () {
+            for (var key in currentQuestion.options) {
+                $answers.append("<li class='question' hidden onclick='nextQuestion(this)' data-type=" + currentQuestion.options[key].type + " data-next=" + currentQuestion.options[key].next + " >" + key + "</li>")
+            }
+            $('.quiz-body').fadeIn('slow', function () {
+                $("#answers ul li").each(function (index) {
+                    $(this).delay(400 * index).fadeIn(300);
+                });
+            });
+
+        });
+    });
+
+});
+
+
+function loadJSON(filename) {
+    var results = {};
+    var filepath = "/sexinfo/data/" + filename;
+    $.ajax({
+        url: filepath,
+        async: false,
+        dataType: 'json',
+        success: function (data) {
+            results = data;
+        }
+    });
+    return results;
+}
+
+function nextQuestion(sender) {
+    var type = $(sender).data('type');
+    var next = $(sender).data('next');
+    if (type == "question") {
+        var question = questions[next];
+        $(sender).siblings().each(function () {
+            $(this).fadeOut('slow');
+        });
+        $quiz.delay(1000).fadeOut('slow', function () {
+            $question.html(question.message);
+            $answers.html("");
+            for (var key in question.options) {
+                $answers.append("<li class='question' hidden onclick='nextQuestion(this)' data-type=" + question.options[key].type + " data-next=" + question.options[key].next + " >" + key + "</li>")
+            }
+            $quiz.fadeIn();
+            $question.fadeIn('slow', function () {
+                $("#answers ul li").each(function (index) {
+                    $(this).delay(400 * index).fadeIn(300);
+                });
+            });
+        });
+        
+    }
+    else {
+        var response = responses[next];
+        $quiz.fadeOut('slow', function () {
+            console.log(response);
+            $response.html("<h2 style='text-align: center'>" + response.title + "</h2>" + "<p style='font-size: 20px;'>" + response.text + "</p>");
+            $response.fadeIn('slow');
+        });
+
+
+    }
+
+}
---
+++ sites/all/themes/magazeen_lite/templates/node--635.tpl.php
@@ -0,0 +1,33 @@
+﻿<div class="terms">
+        <p style="text-align: center;"><b>Terms and Conditions</b></p>
+        <p>Before beginning this questionnaire, please acknowledge your understanding that SexInfo Online cannot diagnose any medical conditions, including pregnancy.</p>
+        
+        <p>The following questionnaire will ask you to respond to detailed questions about your sexual experience concerning male ejaculation, your menstrual cycle, and contraceptive use. Following the inquiry, we will provide you with a response that estimates your probability of pregnancy. The questions may not address your precise situation, but they were designed to encompass the experiences typical of many SexInfo Online readers. Please do your best to choose the answer that most accurately reflects your experience.</p>
+        
+        <p>If you feel the questionnaire did not provide answer choices that accurately reflected your experience or have any other suggestions, please feel free to leave us constructive feedback through the “Ask the Sexperts” submission page!</p>
+</div>
+
+<div class="form-element terms-of-service" id="termsofservice-form-element" style="text-align:center;">
+    <label id="termsofservice-label">
+        <input type="checkbox" value="yes" name="TermsOfService" id="TermsOfService">
+        <span id="terms-of-service-label">
+            <strong>I agree to SexInfo Online's Terms and Conditions. </strong>
+        </span>
+    </label>
+    <span role="alert" class="errormsg" id="errormsg_0_TermsOfService"></span>
+</div>
+
+<a id="start-quiz" class="pink-block-btn" style="width: 250px; text-align: center">Take the quiz</a>
+
+<div class="quiz-body" hidden>
+    <h2 id="question"></h2>
+    <div id="answers">
+        <ul>
+
+        </ul>
+    </div>
+</div>
+
+<div class="response" hidden>
+
+</div>
