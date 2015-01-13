e79f163677f4dcb28a00b6a52926d4d1f8c6f7d1
Mark Nguyen <mpnguyen@umail.ucsb.edu>
2015-01-12 17:16:22 -0800

    Updated responses and terms, added PPQ to front page and nav bar, added image

--- sites/all/themes/magazeen_lite/templates/modules/box_grid.php
+++ sites/all/themes/magazeen_lite/templates/modules/box_grid.php
@@ -19,19 +19,29 @@
 
 
 <div class="third front-module">
-  <h3>Do You Think You Could Be Pregnant?</h3>
-  <p style="font-size: 1.4em; line-height: 1.3;">
-    We've created a brand new pregnancy questionnaire that will help estimate your likelihood of pregnancy.
-    <a href="/sexinfo/ppq">Try it here &raquo;</a>
-  </p>
-</div>
-
-
-<div class="third front-module">
   <h3>Test Your Knowledge</h3>
   <p>Do you think you know it all?</p>
   <p>Are you an expert on masturbation, LGBTQ facts, paraphilias, or pregnancy and abortion?</p>
   <p>Quiz yourself <a href="http://www.soc.ucsb.edu/sexinfo/quizzes">here</a> to see if you really are.</p>
 
   <a class="pink-block-btn" href="/sexinfo/quizzes">Take the quiz</a>
+</div>
+
+
+<div class="third front-module">
+  <h3>Could You Be Pregnant?</h3>
+
+    <div class="faq-image">
+      <a href="http://www.soc.ucsb.edu/sexinfo/question/faq-how-do-i-increase-sexual-arousal">
+      <div style="background: url('<?php print path_to_theme() . '/images/modules/' . '10250738.jpg'; ?>') ; background-size: cover;" class="faq-image-box"></div>
+
+      <div class="caption-slide"></a>
+        <p>We've created a brand new pregnancy questionnaire that will help estimate your likelihood of pregnancy.  <a href="/sexinfo/ppq">Try it here &raquo;</a>
+        </p>
+      </div>
+    </div>
+  <!-- <p style="font-size: 1.4em; line-height: 1.3;">
+    We've created a brand new pregnancy questionnaire that will help estimate your likelihood of pregnancy.
+    <a href="/sexinfo/ppq">Try it here &raquo;</a>
+  </p> -->
 </div>
--- sites/all/themes/magazeen_lite/templates/node--635.tpl.php
+++ sites/all/themes/magazeen_lite/templates/node--635.tpl.php
@@ -2,7 +2,7 @@
         <p style="text-align: center;"><b>Terms and Conditions</b></p>
         <p>Before beginning this questionnaire, please acknowledge your understanding that SexInfo Online cannot diagnose any medical conditions, including pregnancy.</p>
         
-        <p>The following questionnaire will ask you to respond to detailed questions about your sexual experience concerning male ejaculation, your menstrual cycle, and contraceptive use. Following the inquiry, we will provide you with a response that estimates your probability of pregnancy. The questions may not address your precise situation, but they were designed to encompass the experiences typical of many SexInfo Online readers. Please do your best to choose the answer that most accurately reflects your experience.</p>
+        <p>The following questionnaire will ask you to respond to detailed questions about your sexual experience concerning male ejaculation, the female's menstrual cycle, and contraceptive use. Following the inquiry, we will provide you with a response that estimates your probability of pregnancy. The questions may not address your precise situation, but they were designed to encompass the experiences typical of many SexInfo Online readers. Please do your best to choose the answer that most accurately reflects your experience.</p>
         
         <p>If you feel the questionnaire did not provide answer choices that accurately reflected your experience or have any other suggestions, please feel free to leave us constructive feedback through the “Ask the Sexperts” submission page!</p>
 </div>
