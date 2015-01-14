246c84dc958abb1101bd6fa2702c0f0678822c66
Mark Nguyen <mpnguyen@umail.ucsb.edu>
2015-01-13 22:54:05 -0800

    Updated cloud articles

--- sites/all/themes/magazeen_lite/templates/modules/tag_cloud.php
+++ sites/all/themes/magazeen_lite/templates/modules/tag_cloud.php
@@ -4,15 +4,15 @@
   <span class="wrd tagcloud1"><a href="http://www.soc.ucsb.edu/sexinfo/article/semen">ejaculate</a></span>
   <span class="wrd tagcloud6"><a href="http://www.soc.ucsb.edu/sexinfo/article/hymen">hymen</a></span>
   <span class="wrd tagcloud3"><a href="http://www.soc.ucsb.edu/sexinfo/article/sti-symptom-chart">infections</a></span>
-  <span class="wrd tagcloud3"><a href="http://www.soc.ucsb.edu/sexinfo/category/body">body</a></span>
-  <span class="wrd tagcloud1"><a href="http://www.soc.ucsb.edu/sexinfo/taxonomy/term/447/all">violence</a></span>
-  <span class="wrd tagcloud5"><a href="http://www.soc.ucsb.edu/sexinfo/category/love-relationships/love">love</a></span>
-  <span class="wrd tagcloud3"><a href="http://www.soc.ucsb.edu/sexinfo/category/sexual-activity/masturbation">masturbation</a></span>
+  <span class="wrd tagcloud3"><a href="http://www.soc.ucsb.edu/sexinfo/article/sex-determination-and-differentiation">body</a></span>
+  <span class="wrd tagcloud1"><a href="http://www.soc.ucsb.edu/sexinfo/article/sexual-harassment">violence</a></span>
+  <span class="wrd tagcloud5"><a href="http://www.soc.ucsb.edu/sexinfo/article/love-and-attachment">love</a></span>
+  <span class="wrd tagcloud3"><a href="http://www.soc.ucsb.edu/sexinfo/article/female-masturbation">masturbation</a></span>
   <span class="wrd tagcloud1"><a href="http://www.soc.ucsb.edu/sexinfo/article/sexpert-approved-sex-positions">positions</a></span>
-  <span class="wrd tagcloud3"><a href="http://www.soc.ucsb.edu/sexinfo/question/faq-common-questions-about-penis">penis</a></span>
-  <span class="wrd tagcloud6"><a href="http://www.soc.ucsb.edu/sexinfo/questions/pregnancy">pregnancy</a></span>
-  <span class="wrd tagcloud8"><a href="http://www.soc.ucsb.edu/sexinfo/category/sexual-activity/spice-your-sex-life">sex</a></span>
-  <span class="wrd tagcloud1"><a href="http://www.soc.ucsb.edu/sexinfo/category/contraception/barrier-methods">protection</a></span>
+  <span class="wrd tagcloud3"><a href="http://www.soc.ucsb.edu/sexinfo/article/penis-0">penis</a></span>
+  <span class="wrd tagcloud6"><a href="http://www.soc.ucsb.edu/sexinfo/article/signs-and-symptoms-pregnancy">pregnancy</a></span>
+  <span class="wrd tagcloud8"><a href="http://www.soc.ucsb.edu/sexinfo/article/benefits-sex">sex</a></span>
+  <span class="wrd tagcloud1"><a href="http://www.soc.ucsb.edu/sexinfo/article/male-condom">protection</a></span>
   <span class="wrd tagcloud3"><a href="http://www.soc.ucsb.edu/sexinfo/article/overview-female-reproductive-system">vagina</a></span>
   <span class="wrd tagcloud1"><a href="http://www.soc.ucsb.edu/sexinfo/article/effective-communication">communication</a></span>
   <span class="wrd tagcloud3"><a href="http://www.soc.ucsb.edu/sexinfo/article/arguments-and-against-abortion">abortion</a></span>
