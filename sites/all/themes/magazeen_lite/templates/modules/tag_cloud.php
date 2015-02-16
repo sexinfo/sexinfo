455747aaa66a5a72a1010700d01527086b2d13e7
Gabriel Von Meier <vonmeier@umail.ucsb.edu>
2015-02-15 17:45:55 -0800

    fixed broken link sex on front page

--- sites/all/themes/magazeen_lite/templates/modules/tag_cloud.php
+++ sites/all/themes/magazeen_lite/templates/modules/tag_cloud.php
@@ -11,7 +11,7 @@
   <span class="wrd tagcloud1"><a href="http://www.soc.ucsb.edu/sexinfo/article/sexpert-approved-sex-positions">positions</a></span>
   <span class="wrd tagcloud3"><a href="http://www.soc.ucsb.edu/sexinfo/article/penis-0">penis</a></span>
   <span class="wrd tagcloud6"><a href="http://www.soc.ucsb.edu/sexinfo/node/70">pregnancy</a></span>
-  <span class="wrd tagcloud8"><a href="http://www.soc.ucsb.edu/sexinfo/node/545">sex</a></span>
+  <span class="wrd tagcloud8"><a href="http://www.soc.ucsb.edu/sexinfo/node/546">sex</a></span>
   <span class="wrd tagcloud1"><a href="http://www.soc.ucsb.edu/sexinfo/article/male-condom">protection</a></span>
   <span class="wrd tagcloud3"><a href="http://www.soc.ucsb.edu/sexinfo/article/overview-female-reproductive-system">vagina</a></span>
   <span class="wrd tagcloud1"><a href="http://www.soc.ucsb.edu/sexinfo/article/effective-communication">communication</a></span>
