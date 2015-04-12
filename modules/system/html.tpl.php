48fe5cc764cb9e592b6ef55aca71760fce552a08
Mark Nguyen <mpnguyen@umail.ucsb.edu>
2015-04-11 22:11:37 -0700

    Fixed responsive design by including device width

--- modules/system/html.tpl.php
+++ modules/system/html.tpl.php
@@ -47,6 +47,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" version="XHTML+RDFa 1.0" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?>>
 
 <head profile="<?php print $grddl_profile; ?>">
+  <meta name="viewport" content="width=device-width">
   <?php print $head; ?>
   <title><?php print $head_title; ?></title>
   <?php print $styles; ?>
