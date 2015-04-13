ebe0232ec49fbcf11927df46a85e317dcd784fe7
Mark Nguyen <mpnguyen@umail.ucsb.edu>
2015-04-12 21:43:16 -0700

    Touched up front page so carousel is responsive and the heading is more mobile friendly

--- sites/all/themes/magazeen_lite/templates/modules/carousel-frames.php
+++ sites/all/themes/magazeen_lite/templates/modules/carousel-frames.php
@@ -7,14 +7,15 @@ $phpArray = json_decode($jsonData, true);
 
 ?>
 
-<?php foreach ($phpArray as $linkItem) { ?>
-<div class="carousel-frame">
-  <img src="<?php print path_to_theme() . '/images/modules/' . $linkItem['image']; ?>" alt="<?php print $linkItem['alt-text'];?>" />
-  <div class="carousel-caption">
-    <a href="<?php print $linkItem['url'];?>"><h3><?php print $linkItem['title'];?></h3></a>
-    <p><?php print $linkItem['description'];?>
-    <a href="<?php print $linkItem['url'];?>">Read More!</p>
-    </a>
+<?php foreach ($phpArray as $linkItem) {
+    $imagePath = path_to_theme() . '/images/modules/' . $linkItem['image']; ?>
+<div class="carousel-frame" style="background-position: center; background-size: cover; background-image: url('<?php print $imagePath ?>');">
+  <div class="carousel-backdrop">
+      <div class="carousel-caption">
+        <a href="<?php print $linkItem['url'];?>"><h3><?php print $linkItem['title'];?></h3></a>
+        <p><?php print $linkItem['description'];?><a href="<?php print $linkItem['url'];?>">Read More!</a></p>
+        </a>
+      </div>
   </div>
 </div>
 <!-- .carousel.frame -->
