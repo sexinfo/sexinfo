811ac2e3c499497e8c242b8815098cbace8e22de
jeffrey <jeffchen711@icloud.com>
2015-10-13 16:17:41 -0700

    Increased font size of message; cropped image to just be badge

--- sites/all/themes/magazeen_lite/templates/mission.php
+++ sites/all/themes/magazeen_lite/templates/mission.php
@@ -1,8 +1,8 @@
 <div class="front-container" style="margin-top: 20px">
 	<span class="left" style="padding: 10px">
-		<img border="0" align="Right" src="/sexinfo/sites/all/themes/magazeen_lite/images/modules/sex-edu-in-progress.jpg">
+		<img border="0" align="Right" style="padding: 10px" src="/sexinfo/sites/all/themes/magazeen_lite/images/modules/sexinfo-badge.png">
 		<h3>Welcome to SexInfo Online</h3>
-			<p> SexInfo Online is a website devoted to comprehensive sex education based on the best research we have to date. Our primary goal is to
-				ensure that people around the world have access to useful and accurate information about all aspects of human sexuality.</p>
+			<p><font size="4"> SexInfo Online is a website devoted to comprehensive sex education based on the best research we have to date. Our primary goal is to
+				ensure that people around the world have access to useful and accurate information about all aspects of human sexuality.</font></p>
 	</span>
 </div>
