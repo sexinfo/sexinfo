74a7d7756e8b1132c1d14a5812d79ef021b97cf6
asyung <ayung@umail.ucsb.edu>
2015-10-25 16:22:09 -0700

    Changed mission statement card from span to div

--- sites/all/themes/magazeen_lite/templates/mission.php
+++ sites/all/themes/magazeen_lite/templates/mission.php
@@ -1,8 +1,8 @@
-<div class="front-container" style="margin-top: 20px">
-	<span class="left" style="padding: 10px">
-		<img border="0" align="Right" style="padding: 10px" src="/sexinfo/sites/all/themes/magazeen_lite/images/modules/sexinfo-badge.png">
+<div class="front-container" style="margin-top: 20px; padding: 10px; background-image: 
+	url('/sexinfo/sites/all/themes/magazeen_lite/images/modules/mission-bg-cropped.jpg'); max-width:100%; max-height:100%;
+	background-repeat: no-repeat; background-size: 100%; display: block;
+	">
 		<h3>Welcome to SexInfo Online</h3>
-			<p><font size="4"> SexInfo Online is a website devoted to comprehensive sex education based on the best research we have to date. Our primary goal is to
-				ensure that people around the world have access to useful and accurate information about all aspects of human sexuality.</font></p>
-	</span>
+			<p style="font-size: 2vmin">SexInfo Online is a website devoted to comprehensive sex education based on the best research we have to date. Our primary goal is to
+				ensure that people around the world have access to useful and accurate information about all aspects of human sexuality.</p>
 </div>
