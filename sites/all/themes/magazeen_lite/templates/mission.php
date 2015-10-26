40bd4ffd3532cf52e942766fe148aae5b1608326
peterlinlin <peterlin.california@gmail.com>
2015-10-25 22:36:55 -0700

    changed bg and text to resize and read properly

--- sites/all/themes/magazeen_lite/templates/mission.php
+++ sites/all/themes/magazeen_lite/templates/mission.php
@@ -2,7 +2,7 @@
 	url('/sexinfo/sites/all/themes/magazeen_lite/images/modules/mission-bg.jpg'); background-size: cover; display: block;
 	">
 	<div class = "text-on-image" style = "text-shadow: 2px 2px 1px #000000">
-		<h3>Welcome to SexInfo Online</h3>
+		<h3 style = "overflow: visible">Welcome to SexInfo Online</h3>
 			<font size = 3 color = "#ffffff">SexInfo Online is a website devoted to comprehensive sex education based on the best research we have to date. Our primary goal is to
 				ensure that people around the world have access to useful and accurate information about all aspects of human sexuality.</font>
 			</div>
