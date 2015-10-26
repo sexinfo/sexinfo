22056ea34058f5f57d3260692ca8f186f423e809
jeffrey <jeffchen711@icloud.com>
2015-10-26 00:51:01 -0700

    Restructured heading

--- sites/all/themes/magazeen_lite/templates/mission.php
+++ sites/all/themes/magazeen_lite/templates/mission.php
@@ -1,10 +1,19 @@
-<div class="mission-container"
-	style="margin-top: 20px; margin-bottom: 0px; padding: 75px; background-image:
-		url('/sexinfo/sites/all/themes/magazeen_lite/images/modules/mission-bg.jpg');
-		background-size: cover; display: block;
-		">
+<div class="mission-container" style="margin-top: 20px; margin-bottom: 0px; padding: 50px;
+	background-image: url('/sexinfo/sites/all/themes/magazeen_lite/images/modules/mission-bg.jpg');
+	background-size: cover; display: block;">
+
 	<div class="text-on-image" style="padding:20px; background-color: rgba(0,0,0,.5);">
-		<h3><font color="#ffffff">Welcome to <font color="#EC008C">SexInfo Online</h3>
-			<font size=3 color="#ffffff">SexInfo Online is a website devoted to comprehensive sex education based on the best research we have to date. Our primary goal is to ensure that people around the world have access to useful and accurate information about all aspects of human sexuality.</font>
-			</div>
+		<h4>
+			<font color="white">
+				Welcome to
+			</font>
+			<font color="#EC008C">
+				SexInfo Online
+			</font>
+		</h3>
+		<font size=3 color="#ffffff">
+			SexInfo Online is a website devoted to comprehensive sex education based on the best research we have to date. Our primary goal is to ensure that people around the world have access to useful and accurate information about all aspects of human sexuality.
+		</font>
+	</div>
+
 </div>
