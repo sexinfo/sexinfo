8dc965fbde4d2854aa5eb07cf2b9e94ed0a34d87
peterlinlin <peterlin.california@gmail.com>
2015-10-25 22:33:39 -0700

    changed the bg and text to resize correctly and be ledgible

--- sites/all/themes/magazeen_lite/templates/mission.php
+++ sites/all/themes/magazeen_lite/templates/mission.php
@@ -1,8 +1,9 @@
-<div class="front-container" style="margin-top: 20px; padding: 10px; background-image: 
-	url('/sexinfo/sites/all/themes/magazeen_lite/images/modules/mission-bg-cropped.jpg'); max-width:100%; max-height:100%;
-	background-repeat: no-repeat; background-size: 100%; display: block;
+<div class="front-container" style="margin-top: 20px; padding: 75px; background-image:
+	url('/sexinfo/sites/all/themes/magazeen_lite/images/modules/mission-bg.jpg'); background-size: cover; display: block;
 	">
+	<div class = "text-on-image" style = "text-shadow: 2px 2px 1px #000000">
 		<h3>Welcome to SexInfo Online</h3>
-			<p style="font-size: 2vmin">SexInfo Online is a website devoted to comprehensive sex education based on the best research we have to date. Our primary goal is to
-				ensure that people around the world have access to useful and accurate information about all aspects of human sexuality.</p>
+			<font size = 3 color = "#ffffff">SexInfo Online is a website devoted to comprehensive sex education based on the best research we have to date. Our primary goal is to
+				ensure that people around the world have access to useful and accurate information about all aspects of human sexuality.</font>
+			</div>
 </div>
