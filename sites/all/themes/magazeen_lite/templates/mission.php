17047df4320c31116a3e034c472319967cb423b2
jeffrey <jeffchen711@icloud.com>
2015-10-30 14:57:59 -0700

    Centered header, blurred image, slightly darkened body text

--- sites/all/themes/magazeen_lite/templates/mission.php
+++ sites/all/themes/magazeen_lite/templates/mission.php
@@ -1,17 +1,17 @@
 <div class="mission-container" style="margin-top: 20px; margin-bottom: 0px; padding: 50px;
-	background-image: url('/sexinfo/sites/all/themes/magazeen_lite/images/modules/mission-bg.jpg');
+	background-image: url('/sexinfo/sites/all/themes/magazeen_lite/images/modules/mission-bg-blur.jpg');
 	background-size: cover; display: block;">
 
-	<div class="text-on-image" style="padding:20px; background-color: rgba(0,0,0,.5);">
-		<h4>
+	<div class="text-on-image" style="padding:20px; background-color: rgba(0,0,0,.6);">
+		<h4><center>
 			<font color="white">
 				Welcome to
 			</font>
 			<font color="#EC008C">
 				SexInfo Online
 			</font>
-		</h3>
-		<font size=3 color="#ffffff">
+		</center></h4>
+		<font size=3 color="#eeeeee">
 			SexInfo Online is a website devoted to comprehensive sex education based on the best research we have to date. Our primary goal is to ensure that people around the world have access to useful and accurate information about all aspects of human sexuality.
 		</font>
 	</div>
