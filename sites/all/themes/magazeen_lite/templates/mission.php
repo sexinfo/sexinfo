8c07ef4b63a937df9b0adb08768743ab5225fd20
jeffrey <jeffchen711@icloud.com>
2015-11-01 15:23:45 -0800

    Moved mission styles into separate SCSS stylesheet, mission header resizes based on page size

--- sites/all/themes/magazeen_lite/templates/mission.php
+++ sites/all/themes/magazeen_lite/templates/mission.php
@@ -1,19 +1,17 @@
-<div class="mission-container" style="margin-top: 20px; margin-bottom: 0px; padding: 50px;
-	background-image: url('/sexinfo/sites/all/themes/magazeen_lite/images/modules/mission-bg-blur.jpg');
-	background-size: cover; display: block;">
+<div class="mission-container">
 
-	<div class="text-on-image" style="padding:20px; background-color: rgba(0,0,0,.6);">
-		<h4><center>
+	<div class="mission-text">
+		<div class="mission-header"><h4><center>
 			<font color="white">
 				Welcome to
 			</font>
 			<font color="#EC008C">
 				SexInfo Online
 			</font>
-		</center></h4>
-		<font size=3 color="#eeeeee">
+		</center></h4></div>
+		<div class="mission-body"><font>
 			SexInfo Online is a website devoted to comprehensive sex education based on the best research we have to date. Our primary goal is to ensure that people around the world have access to useful and accurate information about all aspects of human sexuality.
-		</font>
+		</font></div>
 	</div>
 
 </div>
