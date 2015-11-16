e479f5c4ec6509a54b62ed10962c063fa39afb0b
jeffrey <jeffchen711@icloud.com>
2015-11-16 01:42:45 -0800

    Fixed slogan on Safari and iOS

--- sites/all/themes/magazeen_lite/templates/header.php
+++ sites/all/themes/magazeen_lite/templates/header.php
@@ -20,7 +20,7 @@
     //print '<h2 class="slogan">'. 'A website devoted sex, health, & relationships.' .'</h2>';
       ?>
     <img src="/sexinfo/sites/default/files/slogan.png" alt="A website devoted to sex, health, and relationships." id="slogan-image"
-      style="margin: 0px 10px; min-height:30%; min-width:30%;"
+      style="margin: 0px 10px; min-width:30%;"
     />
     </div>
     <div id="searchform-header">
