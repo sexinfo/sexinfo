2ec2b3eb76bd145c3995f50559498449adccaaab
jeffrey <jeffchen711@icloud.com>
2016-02-02 16:43:01 -0800

    Added submissionID.js

---
+++ sites/all/themes/magazeen_lite/js/submissionID.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    /*
+     * 1) Detect if on webform submission page
+     * 2) Read submission ID number from page heading
+     * 3) Create new element in "webform-submission" info div
+     */
+
+     console.log("Submission ID JS active");
+})
