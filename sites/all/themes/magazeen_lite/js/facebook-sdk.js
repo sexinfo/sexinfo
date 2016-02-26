9779f63dd59a6105ce0e14568913f72c01727290
asyung <asyung888@gmail.com>
2016-02-26 14:22:59 -0800

    added facebook-sdk javascript

---
+++ sites/all/themes/magazeen_lite/js/facebook-sdk.js
@@ -0,0 +1,9 @@
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5&appId=658178344283309";
+  fjs.parentNode.insertBefore(js, fjs);
+  console.log("facebook sdk imported");
+}(document, 'script', 'facebook-jssdk'));</script>
