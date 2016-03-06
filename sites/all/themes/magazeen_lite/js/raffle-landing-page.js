f0dad006f2a34bd655dca853a773f695b846257f
jeffrey <jeffchen711@icloud.com>
2016-03-05 17:55:16 -0800

    Created CSS file for raffle landing page

--- sites/all/themes/magazeen_lite/js/raffle-landing-page.js
+++ sites/all/themes/magazeen_lite/js/raffle-landing-page.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(window).load(function() {
     var currentLocation = location.pathname;
 
     // Only runs on the view page for a single submission.
@@ -6,11 +6,16 @@ $(document).ready(function() {
         console.log("On Raffle Landing Page");
         var instagrams = $("#block-instagram_block-instagram_block")[0];
         $("#instagram-block").append(instagrams);
-        console.log(instagrams);
-        //$("#block-instagram_block-instagram_block").remove();
-        var twitterFeed = $('#twitter-widget-0')[0];
-        twitterFeed.style.width = "350px";
 
+        /*
+        // Remove the footer from the Twitter feed
+        var twitterFrame = $('#twitter-widget-0')[0].contentDocument;
+        $(twitterFrame).ready(function() {
+            console.log("Twitter iframe loaded");
+            var twitterDoc = $('#twitter-widget-0')[0].contentDocument.children[0];
+            console.log(twitterDoc);
+            $(twitterDoc.children[1].children[0].children[2])[0].remove();
+        })*/
     }
 
 })
