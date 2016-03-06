2ba764d43d46181a4dcc3982210fede97d060fd6
jeffrey <jeffchen711@icloud.com>
2016-03-05 18:24:28 -0800

    Adjusted FB feed padding; Changed Instagram header

--- sites/all/themes/magazeen_lite/js/raffle-landing-page.js
+++ sites/all/themes/magazeen_lite/js/raffle-landing-page.js
@@ -1,11 +1,14 @@
-$(window).load(function() {
+$(document).ready(function() {
     var currentLocation = location.pathname;
 
     // Only runs on the raffle landing page
     if(currentLocation.includes('/sexinfo/raffle-landing-page') === true) {
         //console.log("On Raffle Landing Page");
-        var instagrams = $("#block-instagram_block-instagram_block")[0];
-        $("#instagram-block").append(instagrams);
+        var instagramBlock = $("#block-instagram_block-instagram_block")[0];
+        var instagramHeader = document.createElement('center');
+        instagramHeader.innerHTML = "<h4>Instagram Feed</h4>";
+        $(instagramBlock.children[0]).replaceWith(instagramHeader)
+        $("#instagram-block").append(instagramBlock);
 
         /*
         // Remove the footer from the Twitter feed
