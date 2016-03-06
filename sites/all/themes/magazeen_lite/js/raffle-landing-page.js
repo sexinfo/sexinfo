560916a03a06e7ea6c8702af6f60527e7a9965ca
jeffrey <jeffchen711@icloud.com>
2016-03-05 17:57:55 -0800

    Commented out print statement

--- sites/all/themes/magazeen_lite/js/raffle-landing-page.js
+++ sites/all/themes/magazeen_lite/js/raffle-landing-page.js
@@ -1,9 +1,9 @@
 $(window).load(function() {
     var currentLocation = location.pathname;
 
-    // Only runs on the view page for a single submission.
+    // Only runs on the raffle landing page
     if(currentLocation.includes('/sexinfo/raffle-landing-page') === true) {
-        console.log("On Raffle Landing Page");
+        //console.log("On Raffle Landing Page");
         var instagrams = $("#block-instagram_block-instagram_block")[0];
         $("#instagram-block").append(instagrams);
 
