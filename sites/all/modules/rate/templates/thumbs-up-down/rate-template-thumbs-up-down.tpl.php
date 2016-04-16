c8b7c6e436bbaaf14b3c10a6e62865d871fe0b1b
jeffrey <jeffchen711@icloud.com>
2016-04-15 19:33:20 -0700

    Removed percentages next to thumb rating buttons. Also added 'Thank you' message

--- sites/all/modules/rate/templates/thumbs-up-down/rate-template-thumbs-up-down.tpl.php
+++ sites/all/modules/rate/templates/thumbs-up-down/rate-template-thumbs-up-down.tpl.php
@@ -13,17 +13,18 @@
   </div>
   */
 ?>
-
-<ul>
-  <li class="thumb-up">
-    <?php print $up_button; ?>
-    <div class="percent"><?php print $results['up_percent'] . '%'; ?></div>
-  </li>
-  <li class="thumb-down">
-    <?php print $down_button; ?>
-    <div class="percent"><?php print $results['down_percent'] . '%'; ?></div>
-  </li>
-</ul>
+<center>
+    <ul>
+      <li class="thumb-up">
+        <?php print $up_button; ?>
+        <!--div class="percent"><?php print $results['up_percent'] . '%'; ?></div-->
+      </li>
+      <li class="thumb-down">
+        <?php print $down_button; ?>
+        <!--div class="percent"><?php print $results['down_percent'] . '%'; ?></div-->
+      </li>
+    </ul>
+</center>
 <?php
 
 if ($info) {
