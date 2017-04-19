bc81e1747cdb7b111f7b95b13610d41f77b4bdd3
Ankush Rayabhari <ankush.rayabhari@gmail.com>
2017-04-19 13:27:01 -0700

    Redid navbar entirely and changed various styles

--- sites/all/themes/magazeen_lite/templates/header.php
+++ sites/all/themes/magazeen_lite/templates/header.php
@@ -1,34 +1,29 @@
 <div id="header">
 <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-
+<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/simple-line-icons/2.4.1/css/simple-line-icons.css">
 <?php
   $result     = db_query("select * from {node} where status = 1 and promote = 1 order by rand() limit 1");
   $randomNode = $result->fetch()
 ?>
+  <div id="header-left">
+    <?php include 'navigation.php' ?> 
+  </div>
 
-  <div class="container clearfix">
-    <div id="logo">
-      <?php
-        // Prepare header
-        $site_fields = array();
-        if ($site_name) {
-          $site_fields[] = check_plain($site_name);
-        }
-        $site_title = implode(' ', $site_fields);
-        if ($logo || $site_title) {
-          print '<a href="'. check_url($front_page) .'" title="'. $site_title .'">';
-          if ($logo) {
-            print '<img src="'. check_url($logo) .'" alt="'. $site_title .'" />';
-          }
-          print '</a>';
-        }
-    //print '<h2 class="slogan">'. 'A website devoted to sex, health, & relationships.' .'</h2>';
-      ?>
-      <!--img src="/sexinfo/sites/default/files/slogan.png" alt="A website devoted to sex, health, and relationships." id="slogan-image"/-->
-    </div><!--
+  <div id="header-right">
+      <form action="/sexinfo/" method="post" id="search-block-form" accept-charset="UTF-8">
+      <ul>
+        <li><input placeholder="Search..." id="search-box" type="text" name="search_block_form" maxlength="128" /></li>
+        <li><span onclick="document.getElementById('search-block-form').submit()" data-icon="&#xe090;"></span></li>
+        <li><a class="btn" href="https://www.facebook.com/SexInfoOnline"><span data-icon="&#xe00b;"></span></a></li>
+        <li><a class="btn" href="https://twitter.com/sexinfoonline"><span data-icon="&#xe009;"></span></a></li>
+        <li><a href="https://instagram.com/ucsbsexperts/"><span data-icon="&#58889;"></span></a></li>
+      </ul>
 
-    --><?php include 'navigation.php' ?>
+      <input name="form_build_id" value="form-AmYRLMZDHrIiZu-Dl2zjnB27KK1EN2AC_XZ2YHVi_vE" type="hidden" />
+      <input name="form_id" value="search_block_form" type="hidden" />
+      </form>
+  </div>
 
+  <div style="clear: both"></div>
 
-  </div> <!-- /header-container -->
 </div> <!-- /header -->
